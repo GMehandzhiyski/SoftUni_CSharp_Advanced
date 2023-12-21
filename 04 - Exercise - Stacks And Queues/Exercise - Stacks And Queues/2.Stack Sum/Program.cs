da09b80287e4119ee5b34fd7b0d472485869714d
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-12-21 18:56:38 +0200

    Create  2.Stack_Sum

---
+++ 04 - Exercise - Stacks And Queues/Exercise - Stacks And Queues/2.Stack Sum/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _2.Stack_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
