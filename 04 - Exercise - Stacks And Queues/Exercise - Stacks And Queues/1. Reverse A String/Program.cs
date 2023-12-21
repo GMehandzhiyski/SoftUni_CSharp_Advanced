5a96bb9b7607f27a3c8455ac419983b98e51dfb5
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-12-21 18:53:51 +0200

    Create Exercise - Stacks And Queues

---
+++ 04 - Exercise - Stacks And Queues/Exercise - Stacks And Queues/1. Reverse A String/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _1._Reverse_A_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = Console.ReadLine();
+        
+            Stack<char> stackString = new Stack<char>(inputString);
+
+            foreach (char c in stackString)
+            {
+                Console.Write(c);
+            }
+            Console.WriteLine();
+            while (stackString.Any())
+            {
+                char reuslt = stackString.Pop();
+                Console.Write(reuslt);
+            }
+            ;
+        }
+    }
+}
---
+++ 04 - Exercise - Stacks And Queues/Exercise - Stacks And Queues/Exercise - Stacks And Queues/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercise___Stacks_And_Queues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
