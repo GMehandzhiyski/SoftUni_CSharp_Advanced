1a485e95a166613c13347b7ef747d6e0ab5e55e9
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-02 16:29:37 +0200

    03._Simple_Calculator 100% Done

--- 04 - Exercise - Stacks And Queues/Exercise - Stacks And Queues/3. Simple Calculator/Program.cs
+++ 04 - Exercise - Stacks And Queues/Exercise - Stacks And Queues/3. Simple Calculator/Program.cs
@@ -1,18 +1,38 @@
-﻿namespace _3._Simple_Calculator
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace _3._Simple_Calculator
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int[] inputNumber = Console.ReadLine()
+            string[] inputNumber = Console.ReadLine()
                 .Split(" ",StringSplitOptions.RemoveEmptyEntries)
-                .Select(int.Parse)
                 .ToArray();
 
-            Stack<int> numberStack = new Stack<int>(inputNumber);
+            Stack<string> numberStack = new Stack<string>(inputNumber.Reverse());
+            string sing = string.Empty;
+            int number = 0;
 
+            int result = int.Parse(numberStack.Pop());
 
+           while(numberStack.Any())
+            {
+            
+                sing = numberStack.Pop();
+                number = int.Parse(numberStack.Pop());
 
+                if (sing == "+")
+                {
+                 result += number;
+                }
+                else if (sing == "-")
+                {
+                 result -= number;
+                }
+            }
+
+            Console.WriteLine(result);
         }
     }
 }
