c2048402492286a884ec784ac7d0293452467e67
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-02 21:42:01 +0200

    04.Matching Brackets 100% done

---
+++ 04 - Exercise - Stacks And Queues/Exercise - Stacks And Queues/04.Matching Brackets/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _04.Matching_Brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = Console.ReadLine();
+
+            Stack<int> braketStack = new Stack<int>();
+
+            for (int i = 0; i < inputString.Length; i++)
+           {
+                if (inputString[i] == '(')
+                {
+                    braketStack.Push(i);
+                }
+                else if (inputString[i] == ')')
+                {
+                    int begin = braketStack.Pop();
+                    for (int j = begin; j <= i; j++)
+                    {
+                        Console.Write(inputString[j]);
+                    }
+                    Console.WriteLine();
+                }
+                else 
+                {
+                    continue;
+                }
+                
+            }
+        }
+    }
+}
