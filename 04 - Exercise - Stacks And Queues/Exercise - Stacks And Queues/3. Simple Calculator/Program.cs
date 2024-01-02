6414d091e7ef2d1303e6f0154bf718d6f5fa6a19
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-02 05:39:28 +0200

    continues with ex.03

---
+++ 04 - Exercise - Stacks And Queues/Exercise - Stacks And Queues/3. Simple Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _3._Simple_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputNumber = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> numberStack = new Stack<int>(inputNumber);
+
+
+        }
+    }
+}
