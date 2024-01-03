f58e7c6bf9b215a5191d603313118f1df839f3c1
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-03 06:31:16 +0200

    7. Hot Potato 80% done

---
+++ 04 - Exercise - Stacks And Queues/Exercise - Stacks And Queues/7. Hot Potato/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace _7._Hot_Potato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string[] inputPlayers = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            int hotPottato = int.Parse(Console.ReadLine());
+
+            Queue<string>  playersQueue = new Queue<string>(inputPlayers);
+
+            int cycleCounter = 1;
+            while (playersQueue.Count > 1)
+            { 
+                playersQueue.Enqueue(playersQueue.Dequeue());
+                cycleCounter++;
+                if (cycleCounter == hotPottato)
+                {
+                    string removeHotPottato = playersQueue.Dequeue();
+                    Console.WriteLine($"Removed {removeHotPottato}");
+                    cycleCounter = 1;
+                }
+
+            }
+
+            string finalPlayer = playersQueue.Peek();
+            Console.WriteLine($"Last is {finalPlayer}");
+        }
+    }
+}
