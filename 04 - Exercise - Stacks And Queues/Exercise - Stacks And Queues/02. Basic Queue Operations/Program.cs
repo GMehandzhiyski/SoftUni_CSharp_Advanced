cfb995a2ad5a41d31e100663c31796c14a30805b
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-03 21:09:48 +0200

    02. Basic Queue Operations 100% Done

--- 04 - Exercise - Stacks And Queues/Exercise - Stacks And Queues/02. Basic Queue Operations/Program.cs
+++ 04 - Exercise - Stacks And Queues/Exercise - Stacks And Queues/02. Basic Queue Operations/Program.cs
@@ -4,7 +4,53 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> inputNumber = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            
+            List<int> stringNumber = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            Queue<int> numbers = new Queue<int>();
+
+            int enqueueNumbers = inputNumber[0];
+            int dequeueNumbers = inputNumber[1];
+            int checkNumber = inputNumber[2];   
+
+            for (int i = 0; i < enqueueNumbers; i++) 
+            {
+                numbers.Enqueue(stringNumber[i]);
+            }
+
+            for (int i = 0; i < dequeueNumbers; i++)
+            {
+                numbers.Dequeue();
+            }
+            if (numbers.Count <= 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+            int currNumberLow = int.MaxValue;
+            while (numbers.Any())
+            {
+                int currNumber = numbers.Dequeue();
+                if (currNumber == checkNumber)
+                {
+                    Console.WriteLine("true");
+                    return;
+                }
+                else if (currNumber < currNumberLow ) 
+                {
+                    currNumberLow = currNumber;
+                }
+
+            }
+
+            Console.WriteLine(currNumberLow);
         }
     }
 }
