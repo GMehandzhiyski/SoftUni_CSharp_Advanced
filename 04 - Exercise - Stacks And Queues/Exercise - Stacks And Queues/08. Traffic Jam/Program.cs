bc1e95b3953612a368391941d732ccfa97ce3876
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-03 09:47:49 +0200

    08. Traffic Jam 100% done

--- 04 - Exercise - Stacks And Queues/Exercise - Stacks And Queues/08. Traffic Jam/Program.cs
+++ 04 - Exercise - Stacks And Queues/Exercise - Stacks And Queues/08. Traffic Jam/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int passOnGreen = int.Parse(Console.ReadLine());
+
+            Queue<string> carQueue = new Queue<string>();
+            int cycleToPass = 0;
+            string argumets = string.Empty;
+            int cycleCarPass = 0;
+            while ((argumets = Console.ReadLine()) != "end")
+      {
+                if (argumets == "green")
+                {
+                    while (cycleToPass > 0
+                        && carQueue.Any())
+                    {
+                        string carPass = carQueue.Dequeue();
+                        Console.WriteLine($"{carPass} car passed!");
+                        cycleToPass--;
+                        cycleCarPass++;
+                    }
+                }
+                else
+                {
+                    carQueue.Enqueue(argumets);
+                }
+                 cycleToPass = passOnGreen;
+            }
+
+            Console.WriteLine($"{cycleCarPass} cars passed the crossroads.");
         }
     }
 }
