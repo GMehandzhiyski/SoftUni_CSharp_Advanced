14a44ce76013c943e0d6bde70138c39fb81782d1
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-03 05:58:44 +0200

    5. Print Even Numbers 100% done

--- 04 - Exercise - Stacks And Queues/Exercise - Stacks And Queues/5. Print Even Numbers/Program.cs
+++ 04 - Exercise - Stacks And Queues/Exercise - Stacks And Queues/5. Print Even Numbers/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           int[] inputNumber = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray(); 
+
+
+            Queue<int> numberQueue = new Queue<int>(inputNumber);
+
+            string finalString = string.Empty;
+
+            while (numberQueue.Any())
+            {
+                
+
+                int currNumber = numberQueue.Dequeue();
+                if (currNumber % 2 == 0)
+                {
+                    finalString += currNumber + ", ";
+                }
+
+            }
+            Console.WriteLine(finalString.TrimEnd(',', ' '));
         }
     }
 }
