65814e6fc512e117ca5a9b1b80e3a8b1fa1e52e3
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-03 06:12:54 +0200

    6. Supermarket 100% Done

---
+++ 04 - Exercise - Stacks And Queues/Exercise - Stacks And Queues/6. Supermarket/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _6._Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> nameQueue = new Queue<string>();
+
+            string arguments = string.Empty;
+            while ((arguments = Console.ReadLine()) != "End")
+            {
+                if (arguments == "Paid")
+                {
+                    while (nameQueue.Any())
+                    {
+                        string printName = nameQueue.Dequeue();
+                        Console.WriteLine(printName);
+                    }
+                  
+                }
+                else
+                { 
+                    nameQueue.Enqueue(arguments);
+                }
+            }
+
+            Console.WriteLine($"{nameQueue.Count} people remaining.");
+
+        }
+    }
+}
