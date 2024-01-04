8d565f781a1257ee161a56ebe64d178c8ed60c38
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-04 22:06:30 +0200

    08. Balanced Parenthesis 66% done

--- 04 - Exercise - Stacks And Queues/Exercise - Stacks And Queues/08. Balanced Parenthesis/Program.cs
+++ 04 - Exercise - Stacks And Queues/Exercise - Stacks And Queues/08. Balanced Parenthesis/Program.cs
@@ -4,7 +4,52 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            char[] inputChar = Console.ReadLine()
+                .ToArray();
+
+            Stack<char> symbolStack = new Stack<char>();
+
+            for (int i = 0; i < inputChar.Length; i++)
+            {
+                char currSymbol = inputChar[i];
+                if (currSymbol == '{'
+                    || currSymbol == '('
+                    || currSymbol == '[')
+                {
+                    symbolStack.Push(currSymbol);
+                }
+                else
+                {
+                    char currSymbolInStack = symbolStack.Peek();
+                    if (currSymbolInStack == '{'
+                        && currSymbol == '}')
+                    {
+                        symbolStack.Pop();
+                    }
+                    else if (currSymbolInStack == '('
+                             && currSymbol == ')')
+                    {
+                        symbolStack.Pop();
+                    }
+                    else if (currSymbolInStack == '['
+                             && currSymbol == ']')
+                    {
+                        symbolStack.Pop();
+                    }
+                    else
+                    {
+                        Console.WriteLine("NO");
+                        return;
+                    }
+                }
+            }
+            if (symbolStack.Count > 0)
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+            
+            Console.WriteLine("YES");
         }
     }
 }
