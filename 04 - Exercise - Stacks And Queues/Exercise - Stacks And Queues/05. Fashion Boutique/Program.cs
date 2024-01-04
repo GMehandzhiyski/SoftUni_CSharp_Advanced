c46cdd1fa9fa1da9981b28229dba6792d0b473b2
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-04 09:54:23 +0200

    05. Fashion Boutique 100% done

---
+++ 04 - Exercise - Stacks And Queues/Exercise - Stacks And Queues/05. Fashion Boutique/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.InteropServices;
+
+namespace _05._Fashion_Boutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   //  1   1   1   1   1
+            // 5 4 8 6 3 8 7 7 9
+            // 16
+
+           int[] clothesInBox = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+           int clothes = int.Parse(Console.ReadLine());
+
+            Stack<int> boxes = new Stack<int>(clothesInBox);
+
+            int counterBox = 1;
+            int sumInBox = 0;
+            while (boxes.Any())
+            {
+                int currBox = boxes.Peek();
+                sumInBox += currBox;
+                if (sumInBox <= clothes)
+                {
+                    boxes.Pop();
+                }
+                else
+                { 
+                  counterBox++;
+                  sumInBox = 0;
+                }
+            }
+            Console.WriteLine(counterBox);
+        }
+    }
+}
