f00525d0855d2cc0ea61cfb275cbe9f40dad47f0
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-04 06:34:41 +0200

    04. Fast Food  80% done

--- 04 - Exercise - Stacks And Queues/Exercise - Stacks And Queues/04. Fast Food/Program.cs
+++ 04 - Exercise - Stacks And Queues/Exercise - Stacks And Queues/04. Fast Food/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int quantityFood = int.Parse(Console.ReadLine());
+            int[] orders = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> ordersQueue = new Queue<int>(orders);
+
+            int biggestOrder = int.MinValue;
+            foreach (var currOrder in ordersQueue)
+            {
+                if (biggestOrder < currOrder)
+                { 
+                    biggestOrder = currOrder;
+                }
+            }
+            Console.WriteLine(biggestOrder);
+
+            while (ordersQueue.Any())
+            {
+                int orderComplete = ordersQueue.Dequeue();
+                if (orderComplete > quantityFood)
+                {
+                    Console.WriteLine($"Orders left: {orderComplete}");
+                    return;
+                }
+                quantityFood -= orderComplete;
+                
+            }
+            Console.WriteLine("Orders complete");
         }
     }
 }
