fe0fa9c19abbe67acb7bf1b117ab53754dd414e1
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-05 06:29:16 +0200

    09. Simple Text Editor 25% done

---
+++ 04 - Exercise - Stacks And Queues/Exercise - Stacks And Queues/09. Simple Text Editor/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Data;
+
+namespace _09._Simple_Text_Editor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOperation = int.Parse(Console.ReadLine());
+            string mainString = string.Empty;
+
+            for (int i = 0; i < numberOperation; i++)
+            {
+                string[] commmands = Console.ReadLine()
+                    .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                //Stack<string> textStack = new Stack<string>();
+               
+                string command = commmands[0];
+                int command3Counter = 0;
+
+                if (command == "1")
+                {
+                   mainString += commmands[1];
+                }
+                else if (command == "2")
+                { 
+                
+                }
+                else if (command == "3") 
+                {
+                    int numberOfChar = int.Parse(commmands[1]);
+                    foreach (var currSymbol in mainString)
+                    {
+                        command3Counter++;
+                        if (command3Counter == numberOfChar)
+                        {
+                            Console.WriteLine(currSymbol);
+                            break;
+                        }
+                    }
+                    command3Counter = 0;
+                }
+                else if (command == "4") 
+                { 
+                
+                }
+            }
+        }
+    }
+}
