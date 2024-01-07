99c9591c58ad91801f25ae879ca52a895302cbd7
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-07 09:46:19 +0200

    create new folder exercise

---
+++ 06 - Exercise Multidimensional Arrays/Exercise - Multidimensional Arrays/Exercise - Multidimensional Arrays/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercise___Multidimensional_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
