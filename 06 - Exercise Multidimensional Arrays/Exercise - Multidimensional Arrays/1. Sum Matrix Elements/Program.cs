c05d82c241e527d8e227b5b365c20c01e2c7d810
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-07 09:46:36 +0200

    1. Sum Matrix Elements create

---
+++ 06 - Exercise Multidimensional Arrays/Exercise - Multidimensional Arrays/1. Sum Matrix Elements/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _1._Sum_Matrix_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = numbers[0];
+            int cols = numbers[1];  
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                int[] inputRows = Console.ReadLine()
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i,j] = inputRows[j];  
+                }
+            }
+            int sumOfElelmets = 0;
+            foreach (int row in matrix)
+            {
+                sumOfElelmets += row;   
+            }
+
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            Console.WriteLine(sumOfElelmets);
+
+
+        }
+    }
+}
