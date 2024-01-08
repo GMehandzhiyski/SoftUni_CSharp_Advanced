4822a746d790432d2f17af4280fb4b635dd8b9d6
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-08 06:16:33 +0200

    4. Symbol in Matrix 100% done

--- 06 - Exercise Multidimensional Arrays/Exercise - Multidimensional Arrays/4. Symbol in Matrix/Program.cs
+++ 06 - Exercise Multidimensional Arrays/Exercise - Multidimensional Arrays/4. Symbol in Matrix/Program.cs
@@ -1,10 +1,50 @@
-﻿namespace _4._Symbol_in_Matrix
+﻿using System.Reflection.Metadata;
+
+namespace _4._Symbol_in_Matrix
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int matrixSize = int.Parse(Console.ReadLine());
+
+            string[,] matrix = new string[matrixSize, matrixSize];
+            string[] inputSymbols = new string[matrixSize];
+
+            for (int row = 0; row < matrixSize; row++)
+            {
+                string inputSymbol = Console.ReadLine();
+
+                int counterForEach = 0;
+                foreach (var curSymbol in inputSymbol)
+                {
+                    inputSymbols[counterForEach] = curSymbol.ToString();
+                    counterForEach++;
+
+                }
+
+                for (int col = 0; col < matrixSize; col++)
+                {
+                    matrix[row, col] = inputSymbols[col];
+                }
+            }
+
+            string searchSymbol = (Console.ReadLine());
+
+            string currSymbol = string.Empty;
+            for (int row = 0; row < matrixSize; row++)
+            {
+                for (int col = 0; col < matrixSize; col++)
+                {
+                    currSymbol = matrix[row, col];
+                    if (currSymbol == searchSymbol)
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine($"{searchSymbol}does not occur in the matrix");
         }
     }
 }
