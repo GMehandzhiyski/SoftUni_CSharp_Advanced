59afd229c6ae30f5a16b964667091324c722356f
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-10 06:32:20 +0200

    2. Squares in Matrix 100% done

--- 06 - Exercise Multidimensional Arrays/Exercise - Multidimensional Arrays/2. Squares in Matrix/Program.cs
+++ 06 - Exercise Multidimensional Arrays/Exercise - Multidimensional Arrays/2. Squares in Matrix/Program.cs
@@ -1,10 +1,52 @@
-﻿namespace _2._Squares_in_Matrix
+﻿using System.Runtime.InteropServices;
+
+namespace _2._Squares_in_Matrix
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] matrixSize = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = matrixSize[0];
+            int cols = matrixSize[1];
+
+            string[,] matrix = new string[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                string[] inputNumbers = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = inputNumbers[col];
+                }
+            }
+            int rowsLength = matrix.GetLength(0) - 1;
+            int colsLength = matrix.GetLength(1) - 1;
+            int foundSquares = 0;
+            for (int row = 0; row < rowsLength; row++)
+            {
+                for (int col = 0; col < colsLength; col++)
+                {
+                    string currSymbol = matrix[row, col];
+
+                    if (currSymbol == matrix[row, col + 1]
+                     && currSymbol == matrix[row + 1, col]
+                     && currSymbol == matrix[row + 1, col + 1])
+                    {
+                        foundSquares++;
+                    }
+
+                }
+            }
+
+            Console.WriteLine(foundSquares);
         }
     }
 }
