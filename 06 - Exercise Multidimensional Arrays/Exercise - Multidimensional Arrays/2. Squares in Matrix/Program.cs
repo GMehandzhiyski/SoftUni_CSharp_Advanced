a8185b39b57150d1bc11ca8777b8afb79e60457b
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-10 05:59:19 +0200

    2. Squares in Matrix create

---
+++ 06 - Exercise Multidimensional Arrays/Exercise - Multidimensional Arrays/2. Squares in Matrix/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _2._Squares_in_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
