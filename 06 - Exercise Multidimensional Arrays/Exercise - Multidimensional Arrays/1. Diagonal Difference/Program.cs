f179e5bf6707cba0eafa8aeca224dfc07637337a
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-10 05:56:20 +0200

    1. Diagonal Difference create

---
+++ 06 - Exercise Multidimensional Arrays/Exercise - Multidimensional Arrays/1. Diagonal Difference/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection.Metadata;
+
+namespace _1._Diagonal_Difference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixSize = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[matrixSize, matrixSize];
+
+            for (int row = 0; row < matrixSize; row++)
+            {
+                int[] inputNumbers = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int col = 0; col < matrixSize; col++)
+                {
+                    matrix[row, col] = inputNumbers[col];
+                }
+
+            }
+            int primaryDiagSum = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                primaryDiagSum += matrix[i, i];
+            }
+
+            int secondDiagSum = 0;
+            int colDiag = 0;
+            for (int row = matrix.GetLength(0) - 1; row >= 0; row--)
+            {
+
+                secondDiagSum += matrix[row, colDiag];
+                colDiag++;
+               
+
+            }
+
+            Console.WriteLine(Math.Abs(primaryDiagSum - secondDiagSum));
+         
+        }
+    }
+}
