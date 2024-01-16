8601b6c21f9d4fe2d170fd172c0613ef0123d31c
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-16 06:11:06 +0200

    01.Count Same Values in Array 100% done

---
+++ 08 - Exercise Sets and Dictionaries Advanced/01.Count Same Values in Array/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _01.Count_Same_Values_in_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          List<double> inputString = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToList();
+
+            Dictionary<double, int> valueInArray = new Dictionary<double, int>();
+
+            foreach (var currSymbol in inputString)
+            {
+                if (!valueInArray.ContainsKey(currSymbol))
+                {
+                    valueInArray.Add(currSymbol, 0);
+                }
+                valueInArray[currSymbol]++; 
+            }
+            ;
+
+            foreach (var currSymbol in valueInArray)
+            {
+                Console.WriteLine($"{currSymbol.Key} - {currSymbol.Value} times");
+            }
+        }
+    }
+}
