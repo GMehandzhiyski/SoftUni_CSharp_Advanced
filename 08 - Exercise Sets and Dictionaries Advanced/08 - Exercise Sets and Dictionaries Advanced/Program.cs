610fde231889cd56809c1ba714be3613a2d5c19e
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-16 06:10:46 +0200

    08 - Exercise Sets and Dictionaries Advanced create

---
+++ 08 - Exercise Sets and Dictionaries Advanced/08 - Exercise Sets and Dictionaries Advanced/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _08___Exercise_Sets_and_Dictionaries_Advanced
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
