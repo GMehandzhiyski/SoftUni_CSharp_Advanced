ec64e2954fd20d19739d240f5ac11c7626e47a16
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-16 21:22:58 +0200

    05. Cities by Continent and Country create

---
+++ 08 - Exercise Sets and Dictionaries Advanced/05. Cities by Continent and Country/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _05._Cities_by_Continent_and_Country
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
