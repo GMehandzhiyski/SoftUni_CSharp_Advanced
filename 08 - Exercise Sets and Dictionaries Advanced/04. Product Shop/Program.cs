97c6906b1b32a98aec9445d6e9e78c865bfe7105
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-16 21:21:40 +0200

    04. Product Shop 100% done

--- 08 - Exercise Sets and Dictionaries Advanced/04. Product Shop/Program.cs
+++ 08 - Exercise Sets and Dictionaries Advanced/04. Product Shop/Program.cs
@@ -4,7 +4,50 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           Dictionary<string, Dictionary<string, double>> shops = new Dictionary<string, Dictionary<string, double>>();
+
+
+            string argoment = string.Empty; 
+            while ((argoment = Console.ReadLine()) != "Revision") 
+            {
+                string[] commands = argoment
+                   .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                   .ToArray();
+
+                string shop = commands[0];
+                string product = commands[1];
+                double price = double.Parse(commands[2]);
+
+                if (!shops.ContainsKey(shop))
+                {
+                    shops.Add(shop, new Dictionary<string, double>());  
+                }
+
+                if (!shops[shop].ContainsKey(product))
+                {
+                    shops[shop].Add(product, price);
+                }
+
+                shops[shop][product] = price;
+            }
+             shops = shops.OrderBy(x => x.Key)
+                .ToDictionary(x=>x.Key, x=>x.Value);
+
+            Console.WriteLine();
+
+            foreach (var currShop in shops)
+            {
+                Console.WriteLine($"{currShop.Key}->");
+
+                foreach (var currProduct in currShop.Value)
+                {
+                    Console.Write($"Product: {currProduct.Key}, ");
+                    Console.Write($"Price: {currProduct.Value}");
+                    Console.WriteLine();
+                }
+            }
+
+
         }
     }
 }
