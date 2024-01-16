cad88d7a39731696c9561bdd1af3da57b23a15fc
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-16 21:55:39 +0200

    05. Cities by Continent and Country 60% done

--- 08 - Exercise Sets and Dictionaries Advanced/05. Cities by Continent and Country/Program.cs
+++ 08 - Exercise Sets and Dictionaries Advanced/05. Cities by Continent and Country/Program.cs
@@ -1,10 +1,57 @@
-﻿namespace _05._Cities_by_Continent_and_Country
+﻿using System.Diagnostics;
+
+namespace _05._Cities_by_Continent_and_Country
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int number = int.Parse(Console.ReadLine());
+
+            Dictionary<string, Dictionary<string, List<string>>> continents = new Dictionary<string, Dictionary<string, List<string>>>();
+            for (int i = 0; i < number; i++)
+            {
+                string[] inputString = Console.ReadLine()
+                    .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string continent = inputString[0];
+                string country = inputString[1];
+                string town = inputString[2];
+
+                if (!continents.ContainsKey(continent))
+                {
+                    continents.Add(continent, new Dictionary<string, List<string>>());
+                }
+
+                if (!continents[continent].ContainsKey(country))
+                {
+                    continents[continent].Add(country, new List<string>());
+                }
+
+                if (!continents[continent][country].Contains(town))
+                {
+                    continents[continent][country].Add(town);
+                }
+
+            }
+            
+            foreach (var continent in continents)
+            {
+              
+                Console.WriteLine($"{continent.Key}:");
+                foreach (var country in continent.Value)
+                {
+                    Console.Write($"  {country.Key} -> ");
+                    string finalTown = string.Empty;
+                    foreach (var town in country.Value)
+                    {
+                        finalTown += town + ", ";
+                    }
+                    Console.Write($"{finalTown.TrimEnd(',',' ')}");
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
