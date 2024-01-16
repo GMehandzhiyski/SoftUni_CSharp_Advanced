35ea3edc85a04c9b7766d73ccaf042fece31189e
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-16 06:21:28 +0200

    02. Average Student Grades create

---
+++ 08 - Exercise Sets and Dictionaries Advanced/02. Average Student Grades/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _02._Average_Student_Grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int studentNumber = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < studentNumber; i++)
+            {
+                string[] inputString = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string studentName = inputString[0];
+                double studentGrade = double.Parse(inputString[1]);
+
+                if (!students.ContainsKey(studentName))
+                {
+                   students.Add(studentName, new List<double>());
+                }
+                else
+                {
+                    students[studentName].Add(studentGrade);
+                }
+             }
+        }
+    }
+}
