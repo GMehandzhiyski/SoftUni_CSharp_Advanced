d16ee09830a640013d97fa7c2d272f828001008a
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-17 10:21:56 +0200

    08. SoftUni Party 100% done

--- 08 - Exercise Sets and Dictionaries Advanced/08. SoftUni Party/Program.cs
+++ 08 - Exercise Sets and Dictionaries Advanced/08. SoftUni Party/Program.cs
@@ -4,7 +4,67 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            HashSet<string> vip = new HashSet<string>();  
+            HashSet<string> regular = new HashSet<string>();
+            
+
+            string arguments = string.Empty;
+            bool isPartyTime = false;
+            while ((arguments = Console.ReadLine()) != "END")
+            {
+                bool isFirstCharDigit = CheckFirstChar(arguments);
+               
+                if (arguments == "PARTY")
+                {
+                    isPartyTime = true;
+
+                }
+
+                if (!isPartyTime)
+                {
+                    if (isFirstCharDigit)// VIP
+                    {
+                        vip.Add(arguments);
+                    }
+                    else                //Regular
+                    {
+                        regular.Add(arguments);
+                    }
+                }
+                else 
+                {
+                    if (isFirstCharDigit)
+                    {
+                        vip.Remove(arguments);
+                    }
+                    else
+                    {
+                        regular.Remove(arguments);
+                    }
+
+                }
+
+            }
+            PrintProc(vip, regular);
+
+        }
+
+        private static void PrintProc(HashSet<string> vip, HashSet<string> regular)
+        {
+            Console.WriteLine(vip.Count + regular.Count);
+            foreach (var currVip in vip)
+            {
+                Console.WriteLine(currVip);
+            }
+            foreach (var currRegular in regular)
+            {
+                Console.WriteLine(currRegular);
+            }
+        }
+
+        private static bool CheckFirstChar(string arguments)
+        {
+            return Char.IsDigit(arguments[0]);
         }
     }
 }
