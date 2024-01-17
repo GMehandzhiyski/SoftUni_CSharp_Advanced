86dbcc5363a88f5c246a0e4f789fc70b22052d71
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-17 06:02:15 +0200

    06. Record Unique Names 100% done

---
+++ 08 - Exercise Sets and Dictionaries Advanced/06. Record Unique Names/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _06._Record_Unique_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int number = int.Parse(Console.ReadLine());
+
+            HashSet<string> names = new HashSet<string>();
+
+            for (int i = 0; i < number; i++)
+            {
+                names.Add(Console.ReadLine());  
+            }
+
+            foreach (var currName in names)
+            {
+                Console.WriteLine(currName);
+            }
+        }
+    }
+}
