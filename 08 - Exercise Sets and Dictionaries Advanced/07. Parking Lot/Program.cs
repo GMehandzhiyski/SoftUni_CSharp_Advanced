24693f748ad130e9e141bc038754ce15de4a8ddf
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-17 06:32:51 +0200

    08. SoftUni Party create

--- 08 - Exercise Sets and Dictionaries Advanced/07. Parking Lot/Program.cs
+++ 08 - Exercise Sets and Dictionaries Advanced/07. Parking Lot/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace _07._Parking_Lot
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace _07._Parking_Lot
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            HashSet<string> parkingLot = new HashSet<string>();
+
+            string argumets = string.Empty;
+            while ((argumets = Console.ReadLine()) != "END")
+            {
+                string[] commands = argumets
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                string command = commands[0];
+                string carNumber = commands[1];
+
+                if (command == "IN")
+                {
+                    parkingLot.Add(carNumber);
+                }
+
+                if (command == "OUT")
+                {
+                    parkingLot.Remove(carNumber);
+                }
+
+            }
+
+            if (parkingLot.Count <= 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+                return;
+            } 
+
+            foreach (var currCar in parkingLot)
+            {
+                Console.WriteLine(currCar);
+            }
         }
     }
 }
---
+++ 08 - Exercise Sets and Dictionaries Advanced/08. SoftUni Party/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _08._SoftUni_Party
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
