e659728c656031d08f16189ed51522bb5c3063a8
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-18 06:30:59 +0200

    03. Periodic Table 100% done

---
+++ 08 - Exercise Sets and Dictionaries Advanced/03. Periodic Table/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+int counts = int.Parse(Console.ReadLine());
+SortedSet<string> chemicalElemets = new(); 
+//List<string> chemicalElementsList = new();
+
+for (int i = 0; i < counts; i++)
+{
+    string[] inputString = Console.ReadLine()
+        .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+        .ToArray();
+    foreach (var currElements in inputString)
+    {
+        chemicalElemets.Add(currElements);
+         
+    }
+}
+
+//chemicalElementsList = chemicalElemets.OrderBy(x => x).ToList();
+PrintResult(chemicalElemets);
+
+void PrintResult(SortedSet<string> chemicalElemets)
+{
+    foreach (var currElement in chemicalElemets)
+    {
+        Console.Write($"{string.Join("  ",currElement)} ");
+    }
+}
