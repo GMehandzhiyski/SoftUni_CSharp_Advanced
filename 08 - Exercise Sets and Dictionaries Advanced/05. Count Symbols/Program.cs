57392fa3804d506a035a08013c30d3d81df4783a
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-18 10:14:58 +0200

    05. Count Symbols 100%done

--- 08 - Exercise Sets and Dictionaries Advanced/05. Count Symbols/Program.cs
+++ 08 - Exercise Sets and Dictionaries Advanced/05. Count Symbols/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+SortedDictionary<char, int> symbolsCount = new();
+string inputString = Console.ReadLine();
+
+foreach (var currSymbol in inputString)
+{
+    if (!symbolsCount.ContainsKey(currSymbol))
+    {
+        symbolsCount.Add(currSymbol, 0);
+    }
+    symbolsCount[currSymbol]++;
+}
+PrintResult(symbolsCount);
+
+void PrintResult(SortedDictionary<char, int> symbolsCount)
+{
+    foreach (var currSymbol in symbolsCount)
+    {
+        Console.WriteLine($"{currSymbol.Key} : {currSymbol.Value} time/s");
+    }
+}
