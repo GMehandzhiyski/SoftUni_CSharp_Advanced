c5cf19cda9d58ed3b59b5e9a18052141d09c9afe
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-18 09:56:03 +0200

    04. Even Times 100%done

---
+++ 08 - Exercise Sets and Dictionaries Advanced/04. Even Times/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+int count = int.Parse(Console.ReadLine());
+Dictionary<int, int> numbers = new();
+
+for (int i = 0; i < count; i++)
+{
+    int number = int.Parse(Console.ReadLine());
+    if (!numbers.ContainsKey(number))
+    {
+        numbers.Add(number, 0);
+    }
+    numbers[number]++;
+}
+
+PrintResult(numbers);
+
+void PrintResult(Dictionary<int, int> numbers)
+{
+    foreach (var currNumber in numbers)
+    {
+        if (currNumber.Value % 2 == 0)
+        {
+            Console.WriteLine(currNumber.Key);
+            return;
+        }
+    }
+}
