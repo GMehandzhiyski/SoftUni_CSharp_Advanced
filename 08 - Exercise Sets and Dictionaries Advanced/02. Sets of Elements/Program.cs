cca3fa431860f362f427ad0366419dd4cf7d3933
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-18 06:12:08 +0200

    02. Sets of Elements 100% done

--- 08 - Exercise Sets and Dictionaries Advanced/02. Sets of Elements/Program.cs
+++ 08 - Exercise Sets and Dictionaries Advanced/02. Sets of Elements/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace _02._Sets_of_Elements
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+string[] inputSetLenght = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .ToArray();
+
+int firstSetLenght = int.Parse(inputSetLenght[0]);
+int secondSetLenght = int.Parse(inputSetLenght[1]);
+
+HashSet<int> firstSet = new ();
+HashSet<int> secondSet = new();
+
+for (int i = 0; i < firstSetLenght; i++)
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+    int inputNumber = int.Parse(Console.ReadLine());
+    firstSet.Add(inputNumber);
 }
+
+for (int i = 0; i < secondSetLenght; i++)
+{
+    int inputNumber = int.Parse(Console.ReadLine());
+    secondSet.Add(inputNumber);
+}
+
+firstSet.IntersectWith(secondSet);
+
+Console.WriteLine($"{string.Join(" ",firstSet)}");
