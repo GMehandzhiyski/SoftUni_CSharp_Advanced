2aa39265c8220aa48454a69b09009e7c7e653b81
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-21 09:09:10 +0200

    08. Ranking  60% done

--- 08 - Exercise Sets and Dictionaries Advanced/08 - Exercise Sets and Dictionaries Advanced/Program.cs
+++ 08 - Exercise Sets and Dictionaries Advanced/08 - Exercise Sets and Dictionaries Advanced/Program.cs
@@ -1,10 +1,222 @@
-﻿namespace _08___Exercise_Sets_and_Dictionaries_Advanced
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Xml.Linq;
+namespace LINQDemo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Dictionary<string, string> trainings = new();
+
+            SortedDictionary<string, Dictionary<string, Dictionary<string, int>>> persons = new SortedDictionary<string, Dictionary<string, Dictionary<string, int>>>();
+            //Name             //Course     // Password   //Score
+
+
+            //int score = persons.Where(person => person.Key == "Petko").Select(persons.Values.Where(course => course.Keys.Select(courseName => courseName == "C#").Where(password => password));\\
+            string name = "Petko";
+            string cource = "C#";
+
+            string password = "23456";
+            string password1 = "33456";
+            string password2 = "1@#$%6";
+
+            int score = 50;
+            persons.Add(name, new Dictionary<string, Dictionary<string, int>>());
+            persons[name].Add(cource, new Dictionary<string, int>());
+            persons[name][cource].Add(password, score);
+            persons[name][cource].Add(password1, score);
+            persons[name][cource].Add(password2, score);
+
+
+            var passwordsAndScoreDictonary = persons[name][cource];
+            //.Where(c => c.Key == cource).First()
+            //.Value;
+
+            foreach (var kvp in passwordsAndScoreDictonary.OrderBy(k => k.Key))
+            {
+                Console.WriteLine(kvp.Key);
+            }
+
+
+
+
+            //SortedDictionary<string, Dictionary<string, Dictionary<string, int>>> persons
+            //Name              //Course       // Password  //Score
+
+            //SortedDictionary<string, Dictionary<string, Dictionary<string, int>>> persons
+            //Това
+            string user = persons.Where(person => person.Key == name).Select(p => p.Key).First();
+
+
+            //SortedDictionary<string, Dictionary<string, Dictionary<string, int>>> persons
+            //Това
+            string course = persons.Where(person => person.Key == name)
+                           .Select(person => person.Value
+                                                   .Where(course => course.Key == cource)
+                                                   .Select(course => course.Key).First()).First();
+
+            //SortedDictionary<string, Dictionary<string, Dictionary<string, int>>> persons
+            //Това
+            string pass = persons.Where(person => person.Key == name)
+                                 .Select(person => person.Value
+                                                   .Where(course => course.Key == cource)
+                                                   .Select(course => course.Value
+                                                                            .Where(currPass => currPass.Key == password)
+                                                                            .Select(currPass => currPass.Key).First()).First()).First();
+
+            //SortedDictionary<string, Dictionary<string, Dictionary<string, int>>> persons
+            //Това
+            int points = persons.Where(person => person.Key == name)
+                                 .Select(person => person.Value
+                                                   .Where(course => course.Key == cource)
+                                                   .Select(course => course.Value
+                                                                            .Where(currPass => currPass.Key == password)
+                                                                            .Select(currPass => currPass.Value).First()).First()).First();
+
+
+            //Начално dictonary (примерно - името не е валидно)
+            SortedDictionary<string, Dictionary<string, Dictionary<string, int>>> exampleStartDictonary = new();
+
+            //Взимане на име
+            var personName = persons.Keys.Where(key => key == name).First();
+
+            //Вземане курсoвете на човек
+            Dictionary<string, Dictionary<string, int>> DictonaryOfCurrentPersonCourses = persons[personName];
+            string courseName = DictonaryOfCurrentPersonCourses.Keys.First();
+
+            //Взимане на първата парола
+            Dictionary<string, int> dictonaryOfCurrentPasswordsOfCurrentCourse = DictonaryOfCurrentPersonCourses.Values.First();
+
+            string currentPassword = dictonaryOfCurrentPasswordsOfCurrentCourse.Keys.First();
+
+            int currentScore = dictonaryOfCurrentPasswordsOfCurrentCourse.Values.First();
+
+            Console.WriteLine(courseName);
+
+
+
+
+
+            ////Тук сме взели самият човек                //Tук взимаме курса 
+            //Console.WriteLine(points);
+
+
+            //string arguments = string.Empty;
+            //while ((arguments = Console.ReadLine()) != "end of contests")
+            //{
+            //    string[] token = arguments
+            //        .Split(":", StringSplitOptions.RemoveEmptyEntries)
+            //        .ToArray();
+
+            //    string contest = token[0];
+            //    string password = token[1];
+
+            //    trainings.Add(contest, password);
+            //}
+
+            //while ((arguments = Console.ReadLine()) != "end of submissions")
+            //{
+            //    string[] token = arguments
+            //       .Split("=>", StringSplitOptions.RemoveEmptyEntries)
+            //       .ToArray();
+
+            //    string name = token[2];
+            //    string cource = token[0];
+            //    string password = token[1];
+            //    int score = int.Parse(token[3]);
+
+            //    bool isCourceCorrect = CheckCource(trainings, cource);
+            //    bool isPasswordCorrect = CheckPassword(trainings, password);
+            //    //bool isCourceAndUserIsInclude = CkeckCourceAndUser(persons, name, cource);
+            //    bool isNameIsInclude = CkeckUser(persons, name);
+            //    bool isCourceIsInclude = CkeckCource(persons, password);
+
+
+            //    if (isCourceCorrect
+            //        && isPasswordCorrect)
+            //    {
+            //        if (false)
+            //        {
+
+            //        }
+            //        else
+            //        {
+            //            persons.Add(name, new Dictionary<string, Dictionary<string, int>>());
+            //            persons[name].Add(cource, new Dictionary<string, int>());
+            //            persons[name][cource].Add(password, score);
+            //        }
+
+            //    }
+            //    else
+            //    {
+            //        continue;
+            //    }
+
+
+
+            //}
+
+            //bool CkeckCource(SortedDictionary<string, Dictionary<string, Dictionary<string, int>>> persons, string password)
+            //{
+            //    string matchingEntries = persons
+            //             .Where(n => n.Value.Any(c => c.Value.ContainsKey(password)))
+            //             .ToString();
+
+            //    return true;
+            //}
+
+            //bool CkeckUser(SortedDictionary<string, Dictionary<string, Dictionary<string, int>>> persons, string name)
+            //{
+            //    return
+            //    persons.ContainsKey(name);
+            //}
+
+            //bool CheckPassword(Dictionary<string, string> trainings, string password)
+            //{
+            //    return
+            //        trainings.ContainsValue(password);
+            //}
+            //bool CkeckCourceAndUser(SortedDictionary<string, Dictionary<string, Dictionary<string, int>>> persons, string name, string cource)
+            //{
+            //    //var currPassword = trainings.Where(p => p.Key == name)
+            //    //                       .Select(p => p.Value)
+            //    //                       .Where(d => d.ContainsKey(cource))
+            //    //                       .Select(d => d[cource]);
+            //    //return false;
+            //    return true;
+            //}
+
+            //bool CheckPassword(SortedDictionary<string, Dictionary<string, Dictionary<string, int>>> trainings, string name, string cource, string password)
+            //{
+            //    //foreach (var name in persons)
+            //    //{
+            //    //    foreach (var cource in name.Value)
+            //    //    {
+            //    //        foreach (var pass in cource.Key)
+            //    //        {
+            //    //            if (pass == password)///////////deibbbaaaaaa
+            //    //            {
+            //    //                return true;
+            //    //            }
+            //    //        }
+            //    //    }
+            //    //}
+            //    var currPassword = trainings.Where(p => p.Key == name)
+            //                           .Select(p => p.Value)
+            //                           .Where(d => d.ContainsKey(cource))
+            //                           .Select(d => d[cource])
+            //                           .Where(td => td.ContainsKey(password))
+            //                           .Select(td => td[password]);
+            //    return false;
+            //}
+
+            bool CheckCource(Dictionary<string, string> trainings, string cource)
+            {
+                return
+                     trainings.ContainsKey(cource);
+            }
         }
     }
 }
--- 08 - Exercise Sets and Dictionaries Advanced/08. Ranking/Program.cs
+++ 08 - Exercise Sets and Dictionaries Advanced/08. Ranking/Program.cs
@@ -6,8 +6,7 @@ Algorithms:fun
 end of contests
 C# Fundamentals=>fundPass=>Tanya=>350
 Algorithms=>fun=>Tanya=>380
-Part One
-Interview=>success=>Nikola=>120
+Part One Interview=>success=>Nikola=>120
 Java Basics
 Exam=>JSFundPass=>Parker=>400
 Part One Interview=>success=>Tanya=>220
@@ -15,6 +14,21 @@ OOP Advanced=>password123=>BaiIvan=>231
 C# Fundamentals=>fundPass=>Tanya=>250
 C# Fundamentals=>fundPass=>Nikola=>200
 Js Fundamentals=>JSFundPass=>Tanya=>400
+end of submissions
+
+Java Advanced:funpass
+Part Two Interview:success
+Math Concept:asdasd
+Java Web Basics:forrF
+end of contests
+Math Concept=>ispass=>Monika=>290
+Java Advanced=>funpass=>Simon=>400 
+Part Two Interview=>success=>Drago=>120
+Java Advanced=>funpass=>Petyr=>90 
+Java Web Basics=>forrF=>Simon=>280
+Part Two Interview=>success=>Petyr=>0
+Math Concept=>asdasd=>Drago=>250
+Part Two Interview=>success=>Simon=>200
 end of submissions
  */
 
@@ -24,8 +38,8 @@ using System.Runtime.InteropServices;
 using System.Xml.Linq;
 
 Dictionary<string, string> trainings = new();
-SortedDictionary<string, Dictionary<string, Dictionary<string,int>>> persons = new();
-//               name               cource             pass   score
+SortedDictionary<string,SortedDictionary<string,int>> persons = new();
+//               name             cource  score
 
 string arguments = string.Empty;
 while ((arguments = Console.ReadLine()) != "end of contests")
@@ -45,7 +59,10 @@ while ((arguments = Console.ReadLine()) != "end of submissions")
     string[] token = arguments
        .Split("=>", StringSplitOptions.RemoveEmptyEntries)
        .ToArray();
-
+    if (token.Length < 4)
+    {
+        continue;
+    }
     string name = token[2];
     string cource = token[0];
     string password = token[1];
@@ -53,88 +70,87 @@ while ((arguments = Console.ReadLine()) != "end of submissions")
 
     bool isCourceCorrect = CheckCource(trainings, cource);
     bool isPasswordCorrect = CheckPassword(trainings, password);
-    //bool isCourceAndUserIsInclude = CkeckCourceAndUser(persons, name, cource);
-    bool isNameIsInclude = CkeckUser(persons, name);
-    bool isCourceIsInclude = CkeckCource(persons, password);
-   
-
+    
     if (isCourceCorrect
         && isPasswordCorrect)
     {
-        if (false)
+        bool isNameIsInclude = CkeckUser(persons, name);
+        bool isCourceIsInclude = CkeckCource(persons, name, cource);
+
+        if (isNameIsInclude
+            && isCourceIsInclude)
         {
-            
+            var currscore = persons[name][cource];
+            if (currscore <= score)
+            {
+                persons[name][cource] = currscore;
+            }
         }
-        else
+        else if (isNameIsInclude
+                && !isCourceIsInclude)
         {
-            persons.Add(name, new Dictionary<string, Dictionary<string, int>>());
-            persons[name].Add(cource, new Dictionary<string, int>());
-            persons[name][cource].Add(password, score);
+            persons[name].Add(cource, score);
+        }
+        else 
+        {
+            persons.Add(name, new SortedDictionary<string, int>());
+            persons[name].Add(cource, score);
         }
        
     }
     else
     {
         continue;
+    
+    }
+}
+PrintBestCandidate(persons);
+PirntRanking(persons);
+
+void PirntRanking(SortedDictionary<string, SortedDictionary<string, int>> persons)
+{
+    Console.WriteLine("Ranking:");
+    foreach (var currPerson in persons)
+    {
+        Console.WriteLine(currPerson.Key);
+        foreach (var currInfo in currPerson.Value.OrderByDescending(c => c.Value))
+        {
+            Console.WriteLine($"#  {currInfo.Key} -> {currInfo.Value}");
+        }
+    }
+}
+
+void PrintBestCandidate(SortedDictionary<string, SortedDictionary<string, int>> persons)
+{
+    var topCandidate = persons.OrderByDescending(persons => persons.Value.Sum(cource => cource.Value)).First();
+    Console.WriteLine($"Best candidate is {topCandidate.Key} with total {topCandidate.Value.Sum(sum => sum.Value)} points.");
+}
+
+bool CkeckCource(SortedDictionary<string, SortedDictionary<string, int>> persons, string name, string cource)
+{
+    var currPerson = false;
+    var currCource = false;
+    currPerson = persons.ContainsKey(name);
+    if (currPerson)
+    {
+        return currCource = persons[name].ContainsKey(cource);
     }
 
-   
-
+    return false;
 }
 
-bool CkeckCource(SortedDictionary<string, Dictionary<string, Dictionary<string, int>>> persons,string password)
-{
-    string matchingEntries = persons
-             .Where(n => n.Value.Any(c => c.Value.ContainsKey(password)))
-             .ToString();
- 
-    return true;
-}
-
-bool CkeckUser(SortedDictionary<string, Dictionary<string, Dictionary<string, int>>> persons, string name)
+bool CkeckUser(SortedDictionary<string, SortedDictionary<string, int>> persons, string name)
 {
     return
     persons.ContainsKey(name);
 }
 
-
-//bool CkeckCourceAndUser(SortedDictionary<string, Dictionary<string, Dictionary<string, int>>> persons, string name, string cource)
-//{
-//    //var currPassword = trainings.Where(p => p.Key == name)
-//    //                       .Select(p => p.Value)
-//    //                       .Where(d => d.ContainsKey(cource))
-//    //                       .Select(d => d[cource]);
-//    //return false;
-//    return true;
-//}
 bool CheckPassword(Dictionary<string, string> trainings, string password)
 {
     return
         trainings.ContainsValue(password);
 }
-//bool CheckPassword(SortedDictionary<string, Dictionary<string, Dictionary<string, int>>> trainings, string name, string cource, string password)
-//{
-//    //foreach (var name in persons)
-//    //{
-//    //    foreach (var cource in name.Value)
-//    //    {
-//    //        foreach (var pass in cource.Key)
-//    //        {
-//    //            if (pass == password)///////////deibbbaaaaaa
-//    //            {
-//    //                return true;
-//    //            }
-//    //        }
-//    //    }
-//    //}
-//    var currPassword = trainings.Where(p => p.Key == name)
-//                           .Select(p => p.Value)
-//                           .Where(d => d.ContainsKey(cource))
-//                           .Select(d => d[cource])
-//                           .Where(td => td.ContainsKey(password))
-//                           .Select(td => td[password]);
-//    return false;
-//}
+
 
 bool CheckCource(Dictionary<string, string> trainings, string cource)
 {
@@ -142,8 +158,7 @@ bool CheckCource(Dictionary<string, string> trainings, string cource)
          trainings.ContainsKey(cource);
 }
 
-//SortedDictionary<string, Dictionary<string, Dictionary<string, int>>> persons = new();
-//                  name               cource             pass   score
 
-int totalSum = persons.Sum(m => m.Value.Sum(p => p.Value.Sum(s => s.Value))); 
-Console.WriteLine(totalSum);
+
+
+
