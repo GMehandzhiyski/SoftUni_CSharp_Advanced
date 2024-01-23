c800e61f683a0c826c2adf2098b3f61d042c13de
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-23 11:45:26 +0200

    04. Merge Files 100% done

---
+++ 10 - Exercise Streams, Files and Directories/10 - Exercise Streams, Files and Directories/03. Word Count/Program.cs
@@ -0,0 +1,64 @@
+﻿
+using System.Reflection.PortableExecutable;
+
+namespace WordCount
+{
+    public class WordCount
+    {
+        static void Main()
+        {
+            string wordPath = @"..\..\..\Files\words.txt";
+            string textPath = @"..\..\..\Files\text.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            CalculateWordCounts(wordPath, textPath, outputPath);
+        }
+
+        public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
+        {
+            Dictionary<string, int> keyWords = new();
+            string currWord = String.Empty;
+            List<string> wordsText = new();
+
+            using StreamReader readerWords = new (wordsFilePath);
+            {   
+                while ((currWord = readerWords.ReadLine()) != null)
+                {
+                    string[] token = currWord
+                        .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                        .ToArray();
+                    foreach (var currToken in token)
+                    {
+                        keyWords.Add(currToken, 0);
+                    }
+                   
+                }
+            }
+
+            using StreamReader readerText = new(wordsFilePath);
+            {
+               
+                string argumets = string.Empty;
+
+                while ((argumets = readerText.ReadLine()) != null) 
+                {
+                    string[] toknen =  argumets
+                        .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                        .ToArray();
+
+
+                }
+
+
+
+
+
+                using StreamWriter writer = new(outputFilePath);
+                { 
+                    
+                
+                }
+            }
+        }
+    }
+}
---
+++ 10 - Exercise Streams, Files and Directories/10 - Exercise Streams, Files and Directories/04. 4. Merge Files/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Reflection.PortableExecutable;
+
+namespace MergeFiles
+{
+    public class MergeFiles
+    {
+        static void Main()
+        {
+            var firstInputFilePath = @"..\..\..\Files\input1.txt";
+            var secondInputFilePath = @"..\..\..\Files\input2.txt";
+            var outputFilePath = @"..\..\..\Files\output.txt";
+
+            MergeTextFiles(firstInputFilePath, secondInputFilePath, outputFilePath);
+
+        }
+
+        public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
+        {
+            List<char> firstNumber = new List<char>();
+            List<char> secondNumber = new List<char>();
+            List<char> finalNumber = new List<char>();
+
+            string argumets = string.Empty;
+
+            using StreamReader firstReader = new(firstInputFilePath);
+            {
+                while ((argumets = firstReader.ReadLine()) != null)
+                {
+                    foreach (var currNum in argumets)
+                    {
+                        firstNumber.Add(currNum);
+                    }
+                   
+                }
+                
+            }
+            using StreamReader secondReader = new(secondInputFilePath);
+            {
+                while ((argumets = secondReader.ReadLine()) != null)
+                {
+                    foreach (var currNum in argumets)
+                    {
+                        secondNumber.Add(currNum);
+                    }
+                }
+            }
+            using StreamWriter outputWriter = new (outputFilePath);
+            {
+                int firstCout = 0;
+                int secondCout = 0;
+                for (int i = 0; i < (firstNumber.Count + secondNumber.Count); i++)
+                {
+
+                    if (i % 2 == 0
+                        && firstNumber.Count > firstCout)
+                    {
+                        finalNumber.Add(firstNumber[firstCout]);
+                        firstCout++;
+                    }
+                    else
+                    {
+                        finalNumber.Add(secondNumber[secondCout]);
+                        secondCout++;
+                    }
+                }
+
+
+                for (int i = 0; i < finalNumber.Count; i++)
+                {
+                    outputWriter.WriteLine(finalNumber[i]);
+                }
+            }
+
+        }
+    }
+}
