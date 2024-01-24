a005682c6811bce2af8d36ce9f93c4d5d44278ca
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-24 09:31:20 +0200

    02. Line NumbersEx create

---
+++ 10 - Exercise Streams, Files and Directories/10 - Exercise Streams, Files and Directories/01. Even Lines/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace EvenLines;
+
+using System;
+using System.Data.SqlTypes;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+
+public class EvenLines
+{
+    static void Main()
+    {
+        string inputFilePath = @"..\..\..\text.txt";
+
+        Console.WriteLine(ProcessLines(inputFilePath));
+    }
+
+    public static string ProcessLines(string inputFilePath)
+    {
+        StringBuilder sb = new ();
+
+        using StreamReader reader = new(inputFilePath);
+
+        string line = string.Empty;
+        int lineNumber = 2;
+        while ((line = reader.ReadLine()) != null)
+        {
+            
+            if(lineNumber % 2 == 0)
+            { 
+                string replacedSymbol = ReplacedSymbol(line);
+                string revercedString = RevercedString(replacedSymbol);
+                sb.AppendLine(revercedString);
+            }
+            lineNumber++;
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+    private static string ReplacedSymbol(string line)
+    {
+        char[] symbols = { '-', ',', '.', '!', '?' };
+        foreach (var currsymbol in symbols)
+        {
+            line = line.Replace(currsymbol, '@');
+        }
+       
+        return line;
+    }
+
+    private static string RevercedString(string replacedSymbol)
+    {
+        string[] revercedString = replacedSymbol
+            .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+            .Reverse()
+            .ToArray();
+   
+        
+        return string.Join(" ",revercedString).TrimEnd();
+    }
+
+}
+
