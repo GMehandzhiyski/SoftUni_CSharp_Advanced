60db9c49d1ada209730bdfa2bfe8a6c1adc1bc0f
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-24 05:52:29 +0200

    06. SplitMerge create

--- 10 - Exercise Streams, Files and Directories/10 - Exercise Streams, Files and Directories/05. Extract Specia/Program.cs
+++ 10 - Exercise Streams, Files and Directories/10 - Exercise Streams, Files and Directories/05. Extract Specia/Program.cs
@@ -1,8 +1,11 @@
-﻿namespace ExtractSpecialBytes
+﻿namespace ExtractBytes
 {
-    public class ExtractSpecialBytes
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    public class ExtractBytes
     {
-        static void Main()
+        static void Main(string[] args)
         {
             string binaryFilePath = @"..\..\..\Files\example.png";
             string bytesFilePath = @"..\..\..\Files\bytes.txt";
@@ -13,7 +16,67 @@
 
         public static void ExtractBytesFromBinaryFile(string binaryFilePath, string bytesFilePath, string outputPath)
         {
-            // TODO: write your code here…
+            List<int> bytes = new List<int>();
+            List<int> bytes2 = new List<int>();
+            using (FileStream reader = new FileStream(bytesFilePath, FileMode.Open, FileAccess.Read))
+            {
+                byte[] buffer = new byte[1024];
+
+                while (true)
+                {
+                    int bytesRead = reader.Read(buffer);
+                    if (bytesRead == 0)
+                    {
+                        break;
+                    }
+                    foreach (byte b in buffer)
+                    {
+                        if (b == 0)
+                        {
+                            break;
+                        }
+                        bytes.Add(b);
+                    }
+
+                }
+
+            }
+            using (FileStream reader2 = new FileStream(binaryFilePath, FileMode.Open, FileAccess.Read))
+            {
+                byte[] buffer2 = new byte[1024];
+
+                while (true)
+                {
+                    int bytesRead = reader2.Read(buffer2);
+                    if (bytesRead == 0)
+                    {
+                        break;
+                    }
+                    foreach (byte b in buffer2)
+                    {
+                        if (b == 0)
+                        {
+                            break;
+                        }
+                        bytes2.Add(b);
+                    }
+                }
+            }
+
+            using (FileStream writer = new FileStream(outputPath, FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                foreach (byte byte1 in bytes)
+                {
+                    foreach (byte byte2 in bytes2)
+                    {
+                        if (byte1 == byte2)
+                        {
+                            writer.WriteByte(byte1);
+                           
+                        }
+                    }
+                }
+            }
         }
     }
 }
