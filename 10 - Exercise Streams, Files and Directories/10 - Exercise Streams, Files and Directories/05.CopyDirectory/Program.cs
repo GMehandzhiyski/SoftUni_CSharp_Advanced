493c1fc0e95976f13220fc8c6340e1600593234b
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-25 21:04:02 +0200

    05.CopyDirectory 100% done

--- 10 - Exercise Streams, Files and Directories/10 - Exercise Streams, Files and Directories/05.CopyDirectory/Program.cs
+++ 10 - Exercise Streams, Files and Directories/10 - Exercise Streams, Files and Directories/05.CopyDirectory/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace CopyDirectory
+{
+    using System;
+
+    public class CopyDirectory
+    {
+        static void Main()
+        {
+            string inputPath = @$"D:\SoftUni_CSharp_Advanced\10 - Exercise Streams, Files and Directories\10 - Exercise Streams, Files and Directories\Test";
+            string outputPath = @$"D:\SoftUni_CSharp_Advanced\10 - Exercise Streams, Files and Directories\10 - Exercise Streams, Files and Directories\Test";
+
+            CopyAllFiles(inputPath, outputPath);
+        }
+
+        public static void CopyAllFiles(string inputPath, string outputPath)
+        {
+            if (Directory.Exists(inputPath))
+            { 
+                Directory.Delete(outputPath, true);
+            }
+        }
+    }
+}
