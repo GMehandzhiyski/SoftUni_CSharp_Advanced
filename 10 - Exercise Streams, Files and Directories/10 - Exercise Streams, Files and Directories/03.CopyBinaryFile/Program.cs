af93c6d53a00a0c35bfd8c00d741ea5b7258b5c5
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-25 06:05:34 +0200

    03.CopyBinaryFile 100% done

--- 10 - Exercise Streams, Files and Directories/10 - Exercise Streams, Files and Directories/03.CopyBinaryFile/Program.cs
+++ 10 - Exercise Streams, Files and Directories/10 - Exercise Streams, Files and Directories/03.CopyBinaryFile/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace CopyBinaryFile
+{
+    using System;
+
+    public class CopyBinaryFile
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\copyMe.png";
+            string outputFilePath = @"..\..\..\copyMe-copy.png";
+
+            CopyFile(inputFilePath, outputFilePath);
+        }
+
+        public static void CopyFile(string inputFilePath, string outputFilePath)
+        {
+            using FileStream readerStream = new(inputFilePath, FileMode.Open);
+            using FileStream writerStream = new(outputFilePath, FileMode.Create);
+
+            byte[] buffer = new byte[1024];
+            int size = 0;
+
+           while ((size = readerStream.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                writerStream.Write(buffer, 0, buffer.Length);
+            }
+
+
+
+        }
+    }
+}
