72afc33c1bff2f7b1c41cfcddbb6008b895d16fa
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-25 05:47:21 +0200

    03.CopyBinaryFile create

---
+++ 10 - Exercise Streams, Files and Directories/10 - Exercise Streams, Files and Directories/02.LineNumbersEx/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Xml;
+
+public class LineNumbers
+{
+    static void Main()
+    {
+        string inputFilePath = @"..\..\..\Files\input.txt";
+        string outputFilePath = @"..\..\..\Files\output.txt";
+
+        ProcessLines(inputFilePath, outputFilePath);
+    }
+    public static void ProcessLines(string inputFilePath, string outputFilePath)
+    {
+        StringBuilder sb = new();
+
+        string[] lines = File.ReadAllLines(inputFilePath);
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            int letterCount = lines[i].Count(ch => char.IsLetter(ch));
+            int symbolCount = lines[i].Count(sy => char.IsPunctuation(sy));
+
+            sb.AppendLine($"Line {i + 1}: {lines[i]} ({letterCount})({symbolCount})".TrimEnd());
+            //Console.WriteLine(($"Line {i + 1}: {lines[i]} ({letterCount})({symbolCount})"));
+            using StreamWriter writer = new(outputFilePath);
+            {
+                writer.WriteLine(sb.ToString().TrimEnd());
+            }
+        }
+        
+
+        //File.WriteAllLines(outputFilePath, sb.ToString().TrimEnd()); // thiss function is not working
+
+    }
+}
