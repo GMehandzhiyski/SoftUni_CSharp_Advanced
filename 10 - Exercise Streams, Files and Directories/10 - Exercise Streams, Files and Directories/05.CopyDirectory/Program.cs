8655f2249a677a862d4a5a03d2e5a0278e720aa8
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-26 05:28:49 +0200

    11 - Functional Programming new folder create

--- 10 - Exercise Streams, Files and Directories/10 - Exercise Streams, Files and Directories/05.CopyDirectory/Program.cs
+++ 10 - Exercise Streams, Files and Directories/10 - Exercise Streams, Files and Directories/05.CopyDirectory/Program.cs
@@ -1,22 +1,34 @@
 ﻿namespace CopyDirectory
 {
     using System;
+    using System.Runtime.CompilerServices;
 
     public class CopyDirectory
     {
         static void Main()
         {
-            string inputPath = @$"D:\SoftUni_CSharp_Advanced\10 - Exercise Streams, Files and Directories\10 - Exercise Streams, Files and Directories\Test";
-            string outputPath = @$"D:\SoftUni_CSharp_Advanced\10 - Exercise Streams, Files and Directories\10 - Exercise Streams, Files and Directories\Test";
+            string inputPath = $@"{Console.ReadLine()}";
+            string outputPath = $@"{Console.ReadLine()}";
 
             CopyAllFiles(inputPath, outputPath);
         }
 
         public static void CopyAllFiles(string inputPath, string outputPath)
         {
-            if (Directory.Exists(inputPath))
+            if (Directory.Exists(outputPath))
             { 
-                Directory.Delete(outputPath, true);
+                Directory.Delete(outputPath, recursive:true);
+            }
+
+            Directory.CreateDirectory(outputPath);
+
+            string[] files = Directory.GetFiles(inputPath);
+
+            foreach (var file in files)
+            {
+                string filiName = Path.GetFileName(file);
+                string destination = Path.Combine(outputPath, filiName);
+                File.Copy(file, outputPath) ;
             }
         }
     }
