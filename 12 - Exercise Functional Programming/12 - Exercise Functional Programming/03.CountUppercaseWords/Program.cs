2d364cef249b2b0a1ce381061e37744289f09fb6
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-27 10:03:03 +0200

    03.CountUppercaseWords

---
+++ 12 - Exercise Functional Programming/12 - Exercise Functional Programming/03.CountUppercaseWords/Program.cs
@@ -0,0 +1,13 @@
+﻿string text = Console.ReadLine();
+
+Func<string, bool> check = c => c[0] == c.ToUpper()[0];
+
+string[] finalText = text
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Where(check)
+    .ToArray();
+foreach (var item in finalText)
+{
+    Console.WriteLine(item);
+}
+    
