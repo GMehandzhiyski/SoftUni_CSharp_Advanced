03ef947a0fea29e2d1dc2dc75d0e6b11610cd036
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-27 09:40:06 +0200

    01.SortEvenNumbers 100% done

---
+++ 12 - Exercise Functional Programming/12 - Exercise Functional Programming/01.SortEvenNumbers/Program.cs
@@ -0,0 +1,10 @@
+﻿//4, 2, 1, 3, 5, 7, 1, 4, 2, 12
+
+List<int> number = Console.ReadLine()
+    .Split(", ",StringSplitOptions.RemoveEmptyEntries)
+    .Select(x => int.Parse(x))
+    .Where(x => x % 2 == 0)
+    .OrderBy(x => x)
+    .ToList();
+
+Console.WriteLine(String.Join(", ",number));
