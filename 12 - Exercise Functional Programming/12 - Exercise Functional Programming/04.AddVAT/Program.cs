f2018beeace0afee26ec187e9b81570853438fd4
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-27 10:19:19 +0200

    04.AddVAT 100% done

---
+++ 12 - Exercise Functional Programming/12 - Exercise Functional Programming/04.AddVAT/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Text;
+
+//1.38, 2.56, 4.4
+
+Func<string,double> parse = p => double.Parse(p);
+
+double[] numbers = Console.ReadLine()
+    .Split(", ",StringSplitOptions.RemoveEmptyEntries)
+    .Select(parse)
+    .Select(n => n * 1.2)
+    .ToArray();
+
+foreach (var number in numbers)
+
+{
+    Console.WriteLine($"{number:f3}") ;
+}
+
