1fd6c0b4db2a92b3de5937f89629c2c0368a9251
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-29 11:36:57 +0200

    03.CustomMinFunction 100% done

--- 12 - Exercise Functional Programming/12 - Exercise Functional Programming/03.CustomMinFunction/Program.cs
+++ 12 - Exercise Functional Programming/12 - Exercise Functional Programming/03.CustomMinFunction/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Threading.Channels;
+
+HashSet<int> inputNumbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToHashSet();
+
+Func<HashSet<int>, int> finalNumber = numbers => 
+                        numbers.FirstOrDefault(x => x == inputNumbers.Min());
+//{
+//    int sumOfNUmber = int.MaxValue;
+//    foreach (var number in numbers)
+//    {
+//        if (number < sumOfNUmber)
+//        {
+//            sumOfNUmber = number;
+//        }
+//    }
+    
+//    return sumOfNUmber;
+//};
+int finNumber = 0;
+
+Action<int> printNumber = number => Console.WriteLine(finNumber);
+
+finNumber = finalNumber(inputNumbers);
+printNumber(finNumber);
