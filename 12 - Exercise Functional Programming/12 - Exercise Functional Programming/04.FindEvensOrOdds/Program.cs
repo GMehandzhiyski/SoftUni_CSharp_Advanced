2f8aaa05bd8fcc9ddc6952ba3c78a564008f0d79
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-29 14:25:24 +0200

    05.AppliedArithmetics 100% done

--- 12 - Exercise Functional Programming/12 - Exercise Functional Programming/04.FindEvensOrOdds/Program.cs
+++ 12 - Exercise Functional Programming/12 - Exercise Functional Programming/04.FindEvensOrOdds/Program.cs
@@ -12,15 +12,16 @@ string type = Console.ReadLine();
 List<int> numbers = new List<int>();
 
 // create range
-Func<int,int, List<int>> createList = (startRangee, endRange) =>
-{
-     
-    for (int i = startRangee; i <= endRange; i++)
-    {
-        numbers.Add(i);
-    }
-    return numbers;
-};
+Func<int,int, List<int>> createList = (startRangee, endRange) => 
+            Enumerable.Range(startRange, endRange - startRange + 1).ToList();
+//{
+
+//    for (int i = startRangee; i <= endRange; i++)
+//    {
+//        numbers.Add(i);
+//    }
+//    return numbers;
+//};
 
 // found odd or Even
 Func<List<int>, string, List<int>> oddEvenList = (numbers, type) =>
@@ -29,16 +30,18 @@ Func<List<int>, string, List<int>> oddEvenList = (numbers, type) =>
 
     if (type == "odd")
     {
+        //return numbers.Where(currNumber => currNumber % 2 != 0).ToList();
         foreach (var currNumber in numbers)
         {
             if (currNumber % 2 != 0)
-            { 
+            {
                 currList.Add(currNumber);
             }
         }
     }
     else if (type == "even")
     {
+        //return numbers.Where(currNumber => currNumber % 2 == 0).ToList();
         foreach (var currNumber in numbers)
         {
             if (currNumber % 2 == 0)
@@ -55,8 +58,6 @@ Func<List<int>, string, List<int>> oddEvenList = (numbers, type) =>
     return currList;
 };
 
-
 numbers = createList(startRange, endRange);
 numbers = oddEvenList(numbers, type);
-
 Console.WriteLine(string.Join(" ",numbers));
---
+++ 12 - Exercise Functional Programming/12 - Exercise Functional Programming/05.AppliedArithmetics/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Security.Cryptography.X509Certificates;
+
+List<int> inputNumbers = Console.ReadLine()
+    .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToList();
+
+string arguments = string.Empty;
+while ((arguments = Console.ReadLine()) != "end")
+{
+    string command = arguments;
+
+    Func<List<int>, string, List<int>> listManipulator = (inputNumbers, command) =>
+    {
+       /// List<int> result = new List<int>();
+
+        switch (command)
+        {
+            case "add":
+                return inputNumbers.Select(x => x + 1).ToList();
+                break;
+            case "multiply":
+                return inputNumbers.Select(x => x * 2).ToList();
+                break;
+
+            case "subtract":
+                return inputNumbers.Select(x => x - 1).ToList();
+                break;
+            case "print":
+                Console.WriteLine(string.Join(" ", inputNumbers));
+                break;
+        }
+        return inputNumbers;
+    };
+
+    inputNumbers =  listManipulator (inputNumbers, arguments);  
+}
