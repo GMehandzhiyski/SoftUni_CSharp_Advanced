2c0c7e52e8d1342cb4fcdb4c5ac9515eeeef4bf9
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-29 15:21:29 +0200

    07.PredicateForNames 100% done

--- 12 - Exercise Functional Programming/12 - Exercise Functional Programming/07.PredicateForNames/Program.cs
+++ 12 - Exercise Functional Programming/12 - Exercise Functional Programming/07.PredicateForNames/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int nameLenght = int.Parse(Console.ReadLine());    
+
+List<string> names = Console.ReadLine()
+    .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+    .ToList();
+
+//Predicate<List<string>> checkLengt = names =>
+              // names.All(name => name.Length <= nameLenght);
+
+Func<List<string>,int,List<string>> foundName = (names, nameLenght) =>
+{
+  List<string> result = new List<string>();
+
+    foreach (var currName in names)
+    {
+        if (currName.Length <= nameLenght )
+        { 
+            result.Add(currName);
+        }
+        
+    }
+
+    return result;
+};
+
+names = foundName(names,nameLenght);
+
+Print(names);
+
+void Print(List<string> names)
+{
+    foreach (var currName in names)
+    {
+        Console.WriteLine(currName);
+    }
+}
