21d7b0ed678e7ab070fcd0aae52a2449b31a4bb1
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-29 10:53:50 +0200

    02.KnightsOfHonor create

---
+++ 12 - Exercise Functional Programming/12 - Exercise Functional Programming/02.KnightsOfHonor/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Net.Security;
+
+List<string> inputNames = Console.ReadLine()
+    .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+    .ToList();
+
+Action<List<string>, string> collName = (inputNames, title) =>
+{
+    foreach (string name in inputNames)
+    {
+        Console.WriteLine($"{title} {name}");
+    }
+};
+
+string title = "Sir";
+collName(inputNames, title);
