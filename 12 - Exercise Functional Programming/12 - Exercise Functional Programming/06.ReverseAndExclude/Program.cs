02f3eec651affc08b5d786da68f117338d96a2b7
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-29 14:54:35 +0200

    06.ReverseAndExclude 100% done

---
+++ 12 - Exercise Functional Programming/12 - Exercise Functional Programming/06.ReverseAndExclude/Program.cs
@@ -0,0 +1,41 @@
+﻿
+List<int> inputList = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToList();
+
+int divider = int.Parse(Console.ReadLine());
+
+Predicate<int> checkNumbers = inputList =>
+                    inputList % divider == 0;
+
+Func<List<int>, Predicate<int>, List<int>> foundDivideNumber = (inputList, match) =>
+{
+    List<int> result = new List<int>();
+    foreach (var currNumber in inputList)
+    {
+        if (!match(currNumber))
+        {
+            result.Add(currNumber);
+        }
+    }
+
+
+    return result;
+};
+
+
+Func<List<int>, List<int>> reverceList = inputList =>
+{
+    List<int> result = new ();
+    for (int i = inputList.Count - 1; i >= 0; i--)
+    {
+        result.Add(inputList[i]);
+    }
+
+    return result;
+};
+
+inputList = foundDivideNumber(inputList, checkNumbers);
+inputList = reverceList(inputList);
+Console.WriteLine(string.Join(" ", inputList));
