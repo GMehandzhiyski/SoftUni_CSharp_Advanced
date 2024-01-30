bd8f0cf80e65099335daa69c1dfc3edbac42271d
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-30 12:21:29 +0200

    11.TriFunction100% done

---
+++ 12 - Exercise Functional Programming/12 - Exercise Functional Programming/11.TriFunction/Program.cs
@@ -0,0 +1,27 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+List<string> names = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .ToList();
+
+Func<List<string>, int, string> foundName = (names, number) =>
+{
+    foreach (var name in names)
+    {
+        int nameSum = 0;
+        foreach (var chName in name)
+        {
+            nameSum += chName;
+        }
+
+        if (nameSum >= number)
+        {
+            return name;
+        }
+    }
+    return default;
+};
+
+
+string name = foundName(names, number);
+Console.WriteLine(name);
