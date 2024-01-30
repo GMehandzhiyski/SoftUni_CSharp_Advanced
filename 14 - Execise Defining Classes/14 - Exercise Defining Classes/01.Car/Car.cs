ca444187032f810e9297fd0c37ee44eff859f57b
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-30 16:40:57 +0200

    01.Car 100% done

--- 14 - Execise Defining Classes/14 - Exercise Defining Classes/01.Car/Car.cs
+++ 14 - Execise Defining Classes/14 - Exercise Defining Classes/01.Car/Car.cs
@@ -3,22 +3,20 @@
     class Car
     {
 		private string make;
+        private string model;
+        private int year;
 
-		public string Make
+        public string Make
 		{
 			get { return make; }
 			set { make = value; }
 		}
-
-		private string model;
 
 		public string Model
 		{
 			get { return model; }
 			set { model = value; }
 		}
-
-		private int year;
 
 		public int Year
 		{
