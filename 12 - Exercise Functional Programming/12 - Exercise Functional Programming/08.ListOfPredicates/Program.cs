e7f8f89c911446ca508b0d67a77dc07735b2bac4
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-30 09:46:01 +0200

    08.ListOfPredicates 100% done

--- 12 - Exercise Functional Programming/12 - Exercise Functional Programming/08.ListOfPredicates/Program.cs
+++ 12 - Exercise Functional Programming/12 - Exercise Functional Programming/08.ListOfPredicates/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = int.Parse(Console.ReadLine());
+
+HashSet<int> inputDivider = Console.ReadLine()
+    .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToHashSet();   
+
+List<int> finalList = new List<int>();
+Func<int, List<int>> createList = number =>
+                    Enumerable.Range(1,number).ToList();
+
+
+Func<List<int>, HashSet<int>, List<int>> foundNumbers = (finalList, inputDivider) =>
+{
+    //foreach (var currNumber in finalList)
+    //{
+        
+    //    foreach (var currDivider in inputDivider)
+    //    {
+    //        //if (currNumber % currDivider != 0)
+    //        //{
+    //        //    finalList.Remove(currNumber);
+    //        //    break;
+    //        //}
+           
+    //    }
+    //}
+    finalList = finalList.Where(currNumber =>
+                   inputDivider.All(currDivider => currNumber % currDivider == 0)).ToList();
+    return finalList;
+};
+
+finalList = createList(number);
+finalList = foundNumbers(finalList, inputDivider);
+Console.WriteLine(string.Join(" ", finalList));
