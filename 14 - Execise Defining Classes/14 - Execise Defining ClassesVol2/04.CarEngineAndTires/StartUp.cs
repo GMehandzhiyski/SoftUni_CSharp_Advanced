5967f81bfa2f2efdb32c6acb707950a0c3b1531e
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-01-31 13:18:41 +0200

    same ex new project

---
+++ 14 - Execise Defining Classes/14 - Execise Defining ClassesVol2/04.CarEngineAndTires/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+
+            Tire[] tires =
+            {
+                new Tire(1, 2.5),
+                new Tire(1, 2.1),
+                new Tire(2, 0.5),
+                new Tire(2, 2.3),
+            };
+            
+            var engine = new Engine(560, 6300);
+
+            var car = new Car("Lamborghini", "Urus", 2010, 250, 9, engine, tires);
+
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
