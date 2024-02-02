53615e0b43f93880dc1ff8237035358a255bdd78
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-02-02 16:03:37 +0200

    01.DefineaClassPerson 100% done

---
+++ 14 - Execise Defining Classes/14 - Exercise Defining ClassesVol3/01.DefineaClassPerson/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {   List<Person> persons = new List<Person>();
+            Person person = new Person();
+            person.Name = "Peter";
+            person.Age = 20;
+            persons.Add(person);
+            person.Name = "Geroge";
+            person.Age = 18;
+            persons.Add(person);
+            person.Name = "Jose";
+            person.Age = 43;
+            persons.Add(person);
+            ;
+        }
+    }
+}
+
