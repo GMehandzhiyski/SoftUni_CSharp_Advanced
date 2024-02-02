4f1acfe8ef2fb0b05a91a22177cf3c4e5ef0d4bd
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-02-02 12:08:03 +0200

    03.CarConstructors 100% done

--- 14 - Execise Defining Classes/14 - Exercise Defining ClassesVol3/02.CarExtension/StartUp.cs
+++ 14 - Execise Defining Classes/14 - Exercise Defining ClassesVol3/02.CarExtension/StartUp.cs
@@ -10,7 +10,14 @@ namespace CarManufacturer
     {
         static void Main()
         {
-         
+            Car car = new();
+            car.Model = "VW";
+            car.Make = "MK3";
+            car.Year = 1992;
+            car.FuelQuantity = 200;
+            car.FuelConsumption = 200;
+            car.Drive(2000);
+            Console.WriteLine(car.WhoAmI());
         }
     }
 }
