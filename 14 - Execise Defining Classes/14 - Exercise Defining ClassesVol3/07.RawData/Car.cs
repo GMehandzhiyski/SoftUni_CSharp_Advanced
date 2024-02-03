dab65ffad96626aa7b467db2aa026dfd49813318
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-02-03 16:41:42 +0200

    07.RawData create

---
+++ 14 - Execise Defining Classes/14 - Exercise Defining ClassesVol3/07.RawData/Car.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Car
+    {
+		private string model;
+		private double fuelAmount;
+		private double fuelConsumptionPerKilometer;
+		private double travelledDistance;
+
+        public Car()
+        {
+				
+        }
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKilometer, double travelledDistance)
+			
+        {
+            Model = model;
+            FuelAmount = fuelAmount;
+            FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+           
+        }
+
+        public string Model
+		{
+			get { return model; }
+			set { model = value; }
+		}
+
+		public double FuelAmount
+        {
+			get { return fuelAmount; }
+			set { fuelAmount = value; }
+		}
+
+		public double FuelConsumptionPerKilometer
+        {
+			get { return fuelConsumptionPerKilometer; }
+			set { fuelConsumptionPerKilometer = value; }
+		}
+
+		public double TravelledDistance
+        {
+			get { return travelledDistance; }
+			set { travelledDistance = value; }
+		}
+
+		public void DriveCar(double amountOfKm)
+		{
+            double movingRange = amountOfKm * fuelConsumptionPerKilometer;
+            if (movingRange <= FuelAmount)
+            {
+                fuelAmount -= movingRange;
+                travelledDistance += amountOfKm;
+            }
+            else 
+			{
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+		
+		}
+
+
+
+
+
+	}
+}
+
---
+++ 14 - Execise Defining Classes/14 - Exercise Defining ClassesVol3/07.RawData/StartUp.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {   
+            List<Car> cars = new();
+            int numberCar = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberCar; i++)
+            {
+                string[] carInput = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string model = carInput[0];
+                int fuelAmount = int.Parse(carInput[1]); 
+                double fuelConsumptionFor1km = double.Parse(carInput[2]);
+
+                Car car = new(model, fuelAmount, fuelConsumptionFor1km,0.0);
+                if (!cars.Contains(car))
+                {
+                    cars.Add(car);
+                }
+               
+            }
+            string arguments = string.Empty;
+            while ((arguments = Console.ReadLine()) != "End")
+            {
+                string[] carInput = arguments
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                
+                    string command = carInput[0];
+                    string model = carInput[1];
+                    double amountOfKm = double.Parse(carInput[2]);
+
+                Car carDriving = cars
+                  .Where(x => x.Model == model)
+                  .First();
+
+                if (command == "Drive")
+                {
+                    carDriving.DriveCar(amountOfKm);
+                }
+            }
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine($"{car.Model} {car.FuelAmount:f2} {car.TravelledDistance}");
+            }
+        }
+    }
+}
+
