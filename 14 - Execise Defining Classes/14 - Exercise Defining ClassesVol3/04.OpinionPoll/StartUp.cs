ceb786bc039949e03880f7311d35e077cc99a7e1
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-02-03 09:34:40 +0200

    04.OpinionPoll create

---
+++ 14 - Execise Defining Classes/14 - Exercise Defining ClassesVol3/04.OpinionPoll/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {   //List<Person> persons = new List<Person>();
+            //Person person = new Person();
+            //person.Name = "Peter";
+            //person.Age = 20;
+            //persons.Add(person);
+            //person.Name = "Geroge";
+            //person.Age = 18;
+            //Console.WriteLine($"{person.Name} {person.Age}");
+            //persons.Add(person);
+            //person.Name = "Jose";
+            //person.Age = 43;
+            //Console.WriteLine($"{person.Name} {person.Age}");
+            //persons.Add(person);
+            //Person person = new Person("Invan", 20);
+
+            //Family family = new();
+            //family.AddMember(person);
+
+            int numberPeople = int.Parse(Console.ReadLine());
+            Family family = new Family();
+            for (int i = 0; i < numberPeople; i++)
+            {
+                string[] nameAndAge = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string name = nameAndAge[0];
+                int age = int.Parse(nameAndAge[1]); 
+
+                Person  person = new Person(name,age);
+              
+                family.AddMember(person);
+              
+            }
+            
+            Person oldestMember = family.GetOldestMember();
+            Console.WriteLine($"{ oldestMember.Name} {oldestMember.Age}");
+
+        }
+    }
+}
+
