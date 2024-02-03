7cd476c7a08a8368448766653075cba7ecf3b786
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-02-03 09:32:00 +0200

    03.OldestFamilyMember 100% done

---
+++ 14 - Execise Defining Classes/14 - Exercise Defining ClassesVol3/03.OldestFamilyMember/Family.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+		private List<Person> persons;
+
+        public Family()
+        {
+            persons = new List<Person>();
+        }
+
+        public List<Person> Persons
+		{
+			get { return persons; }
+			set { persons = value; }
+		}
+
+		public void AddMember(Person members)
+		{
+			persons.Add(members);
+		}
+
+		public Person GetOldestMember()
+		{
+			Person oldestPerson = persons
+				.OrderByDescending(p => p.Age).FirstOrDefault();
+			return oldestPerson;
+		}
+	}
+}
