521b5482b6c544c4b02ad54da52c817345f2b813
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-02-04 15:39:48 +0200

    09.PokemonTrainer 100% DONE

---
+++ 14 - Execise Defining Classes/14 - Exercise Defining ClassesVol3/09.PokemonTrainer/StartUp.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            List<Trainers> trainers = new();
+
+            string arguments = string.Empty;
+            while ((arguments = Console.ReadLine()) != "Tournament")
+            {
+                string[] token = arguments
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string trainerName = token[0];
+                string pokemonName = token[1];
+                string pokemonElements = token[2];
+                int pokemonHealth = int.Parse(token[3]);
+
+                Pokemon pokemon = new Pokemon(pokemonName, pokemonElements, pokemonHealth);
+                if (!trainers
+                    .Any(t => t.Name == trainerName))
+                {
+                    Trainers trainer = new Trainers(trainerName);
+                    trainer.Pokemons.Add(pokemon);
+                    trainers.Add(trainer);
+                }
+                else
+                {
+                    Trainers currTrainer = trainers
+                           .Where(t => t.Name == trainerName).FirstOrDefault();
+                    currTrainer.Pokemons.Add(pokemon);
+                    //trainers.Add(currTrainer);
+                }
+            }
+
+            while ((arguments = Console.ReadLine()) != "End")
+            {
+                if (!(arguments == "Fire"
+                    || arguments == "Water"
+                    || arguments == "Electricity"))
+                {
+                    continue;
+                }
+                foreach (var currTrainer in trainers)
+                {
+                    if (currTrainer.Pokemons
+                        .Any(e => e.Element == arguments))
+                    {
+                        currTrainer.Bages += 1;
+                    }
+                    else
+                    {
+                        foreach (var currPokemons in currTrainer.Pokemons)
+                        {
+                            currPokemons.Health -= 10;
+                        }
+
+                        currTrainer.Pokemons.RemoveAll(h => h.Health <= 0);
+                    }
+                }
+            }
+
+            foreach (var currTrainers in trainers.OrderByDescending(b => b.Bages))
+            {
+                Console.WriteLine($"{currTrainers.Name} {currTrainers.Bages} {currTrainers.Pokemons.Count}");
+            }
+        }
+    }
+}
+
---
+++ 14 - Execise Defining Classes/14 - Exercise Defining ClassesVol3/09.PokemonTrainer/Trainers.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Trainers
+    {
+		private string name;
+		private int bages;
+		private List<Pokemon> pokemons = new List<Pokemon>();
+
+        public Trainers()
+        {
+			pokemons = new List<Pokemon>();
+        }
+
+        public Trainers(string name)
+        {
+            Name = name;
+            Bages = 0;
+            Pokemons = new List<Pokemon>();
+        }
+
+        public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		public int Bages
+        {
+			get { return bages; }
+			set { bages = value; }
+		}
+
+		public List<Pokemon> Pokemons
+        {
+			get { return pokemons; }
+			set { pokemons = value; }
+		}
+
+	}
+}
+
