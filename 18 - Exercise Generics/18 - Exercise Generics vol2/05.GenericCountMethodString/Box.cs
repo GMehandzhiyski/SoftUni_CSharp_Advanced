943fca50b6e0167f3cb975756c29e300ad22265d
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-02-06 07:23:29 +0200

    05.GenericCountMethodString create

---
+++ 18 - Exercise Generics/18 - Exercise Generics vol2/05.GenericCountMethodString/Box.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericBoxOfString
+{
+    public class Box<T> where T : IComparable<T>
+    {
+        private List<T> items;
+        private int count;
+
+        public Box()
+        {
+            items = new List<T>();
+        }
+
+        public int Count
+        {
+            get { return count; }
+            //set { count = value; }
+        }
+
+        public void Add(T item) 
+        {
+            items.Add(item);
+        }
+
+     public void Copmare(T element) 
+     {
+            foreach (var item in items)
+            {
+                if (item.CompareTo(element) > 0) 
+                {
+                    count++;
+                }
+            }
+        }
+    }
+}
---
+++ 18 - Exercise Generics/18 - Exercise Generics vol2/05.GenericCountMethodString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+
+namespace GenericBoxOfString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            Box<string> box = new Box<string>();
+            for (int i = 0; i < number; i++)
+            {
+                string item = (Console.ReadLine());
+
+                box.Add(item);
+            }
+
+            string element = (Console.ReadLine());
+            box.Copmare(element);
+            Console.WriteLine(box.Count);    
+        }   
+    }
+}
