13a4177e0de377d973fd16350105b6ae35fa57f2
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-02-06 06:29:57 +0200

    01.GenericBoxOfString 100% done

---
+++ 18 - Exercise Generics/18 - Exercise Generics vol2/01.GenericBoxOfString/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace GenericBoxOfString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            Box<string> box = new Box<string>();
+
+            for (int i = 0; i < number; i++)
+            {
+                string item = Console.ReadLine();
+
+                  
+
+                box.Add(item);
+            }
+
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
