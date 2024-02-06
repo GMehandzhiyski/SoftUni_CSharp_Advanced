3412d77456b7b6a242a044b681bd187ade7d781c
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-02-06 06:55:49 +0200

    03.GenericSwapMethodS tring 100% done

---
+++ 18 - Exercise Generics/18 - Exercise Generics vol2/03.GenericSwapMethodS tring/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace GenericBoxOfString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> items = new List<string>();
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < number; i++)
+            {
+                string item = Console.ReadLine();
+
+                items.Add(item);
+            }
+
+            int[] token = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+           items =  SwapList(items, token[0], token[1]);
+
+            PrintResult(items);
+        }
+        private static List<T> SwapList<T>(List<T> items, int token1, int token2)
+        {
+            (items[token1], items[token2]) = (items[token2], items[token1]);
+
+            return items;
+        }
+
+        private static void PrintResult<T>(List<T> items)
+        {
+
+            foreach (var item in items)
+            {
+                Console.WriteLine($"{item.GetType()}: {item}");
+            }
+        }
+    }
+}
