d882df945456bc5a308dc37a0af8c545bf3bdca5
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-02-06 08:22:40 +0200

    08.Threeuple 100% done

---
+++ 18 - Exercise Generics/18 - Exercise Generics vol2/07.Tuple/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+
+namespace GenericBoxOfString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] name = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string[] beer = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string[] number = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            CustomTuple<string, string> names = new($"{name[0]} {name[1]}", name[2]);
+
+            CustomTuple<string, int> beers = new(beer[0], int.Parse(beer[1]));
+
+            CustomTuple<double, double> numbers = new(double.Parse(number[0]), double.Parse(number[1]));
+
+            Console.WriteLine(names);
+            Console.WriteLine(beers);
+            Console.WriteLine(numbers);
+        }   
+    }
+}
---
+++ 18 - Exercise Generics/18 - Exercise Generics vol2/08.Threeuple/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+
+namespace GenericBoxOfString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] name = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string[] beer = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string[] number = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            CustomTuple<string, string, string> names = new($"{name[0]} {name[1]}", name[2], string.Join(" ",name[3..]));
+
+            CustomTuple<string, int, bool> beers = new(beer[0], int.Parse(beer[1]), beer[2] == "drunk");
+
+            CustomTuple<string, double, string> numbers = new((number[0]), double.Parse(number[1]), number[2]);
+
+            Console.WriteLine(names);
+            Console.WriteLine(beers);
+            Console.WriteLine(numbers);
+        }   
+    }
+}
