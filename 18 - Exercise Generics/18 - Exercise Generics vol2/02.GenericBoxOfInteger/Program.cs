d1922a91499028d8e02d4dfec38c43d737f57258
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-02-06 06:34:57 +0200

    02.GenericBoxOfInteger create

---
+++ 18 - Exercise Generics/18 - Exercise Generics vol2/02.GenericBoxOfInteger/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace GenericBoxOfString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            Box<int> box = new Box<int>();
+
+            for (int i = 0; i < number; i++)
+            {
+                int item = int.Parse(Console.ReadLine());
+
+                  
+
+                box.Add(item);
+            }
+
+            Console.WriteLine(box.ToString());
+        }   
+    }
+}
