877afe854b85cdbb6e5b69b54a1338092c9e1f48
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-02-08 21:52:40 +0200

    06.Supermarket 100% done

---
+++ 21 - Exam Preparation/21 - Exam Preparation/06.Supermarket/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Queue<string> queue = new Queue<string>();
+string arguments = string.Empty; 
+while ((arguments = Console.ReadLine()) != "End" )
+{
+    if (arguments == "Paid")
+    {
+        while (queue.Any())
+        {
+            Console.WriteLine(queue.Dequeue());
+           
+        }
+        continue;
+    }
+
+    queue.Enqueue(arguments);
+}
+    Console.WriteLine($"{queue.Count} people remaining.");
