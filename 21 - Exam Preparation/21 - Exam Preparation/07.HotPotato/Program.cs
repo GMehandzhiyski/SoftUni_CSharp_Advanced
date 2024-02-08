8f26fcd504825dece277387b0633a72b940c7bab
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-02-08 22:13:35 +0200

    07.HotPotato 100% done

--- 21 - Exam Preparation/21 - Exam Preparation/07.HotPotato/Program.cs
+++ 21 - Exam Preparation/21 - Exam Preparation/07.HotPotato/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+string[] inputName = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+int potatoNumber = int.Parse(Console.ReadLine());
+
+Queue<string> queueName = new Queue<string>(inputName);
+int countRotation = 1;
+while (queueName.Count != 1)
+{
+   while(countRotation < potatoNumber)
+    {
+        countRotation++;
+        string name = queueName.Dequeue();
+        queueName.Enqueue(name);
+       
+    }
+    Console.WriteLine($"Removed {queueName.Dequeue()}");
+    countRotation = 1;
+
+}
+
+Console.WriteLine($"Last is {queueName.Dequeue()}");
