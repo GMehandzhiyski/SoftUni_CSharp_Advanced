78a355c911c69c3e0da8b71bfe989e860e4b4dbc
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-02-08 21:40:20 +0200

    5.PrintEvenNumbers 100% done

---
+++ 21 - Exam Preparation/21 - Exam Preparation/5.PrintEvenNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+int[] inputNumber = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+Queue<int> queue = new(inputNumber);
+string result = string.Empty;
+while (queue.Any())
+{
+   int number = queue.Dequeue();
+
+   
+    if (number % 2 == 0)
+    {
+      result += number + ", ";
+    }
+}
+Console.WriteLine(result.TrimEnd(',', ' '));
