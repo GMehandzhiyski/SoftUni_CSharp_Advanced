5bb0db7698f4a098373465db6978bcb7fe2b5449
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-02-08 20:08:08 +0200

    2.StackSum 100% done

---
+++ 21 - Exam Preparation/21 - Exam Preparation/1.ReverseAString/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+//Stack<char> stack = new();
+
+//string inputString = Console.ReadLine();
+
+//foreach (var itemChar in inputString)
+//{
+//    stack.Push(itemChar);
+//}
+//StringBuilder sb = new StringBuilder();
+//foreach (var chars in stack)
+//{
+//    sb.Append(chars);
+//}
+
+//Console.WriteLine(sb);\
+
+Stack<char> stack = new();
+
+string inputString = Console.ReadLine();
+
+foreach (var itemChar in inputString)
+{
+    stack.Push(itemChar);
+}
+
+string finalString = string.Empty;
+while (stack.Any())
+{
+   finalString += stack.Pop().ToString();
+}
+
+Console.WriteLine(finalString);
---
+++ 21 - Exam Preparation/21 - Exam Preparation/2.StackSum/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+
+int[] inputNumbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+Stack<int> stack = new Stack<int>(inputNumbers);    
+
+string arguments = string.Empty;
+while ((arguments = Console.ReadLine().ToLower()) != "end")
+{
+    string[] commands = arguments
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    string command = commands[0];
+
+    if (command == "add")
+    {
+        int numberOne = int.Parse(commands[1]);  
+        int numberTwo = int.Parse(commands[2]); 
+        stack.Push(numberOne);
+        stack.Push(numberTwo);
+    }
+
+    if (command == "remove")
+    {
+        int removeNumber = int.Parse(commands[1]);
+
+        for (int i = 0; i < removeNumber && stack.Count >= removeNumber; i++)
+        {
+            stack.Pop();
+        }
+    }
+}
+Console.WriteLine(stack.Sum());
