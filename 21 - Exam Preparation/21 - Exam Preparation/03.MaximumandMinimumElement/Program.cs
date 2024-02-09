848181bef40b88ae13754671f8495fc17a8c6ac1
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-02-09 06:39:06 +0200

    03.MaximumandMinimumElement 100% done

--- 21 - Exam Preparation/21 - Exam Preparation/03.MaximumandMinimumElement/Program.cs
+++ 21 - Exam Preparation/21 - Exam Preparation/03.MaximumandMinimumElement/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+int numberCycles = int.Parse(Console.ReadLine());
+Stack<int> stack = new Stack<int>();
+
+for (int i = 0; i < numberCycles; i++)
+{
+    int[] commands = Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .ToArray();
+
+    int command = commands[0];
+
+    if (command == 1)
+    {
+        int number = commands[1];
+        stack.Push(number);
+    }
+    else if (command == 2)
+    {
+        stack.Pop();
+    }
+    else if (command == 3
+        && stack.Any())
+    {
+        int maxElement = stack.Max();
+        Console.WriteLine(maxElement);
+    }
+    else if (command == 4
+         && stack.Any())
+    {
+        int minElement = stack.Min();
+        Console.WriteLine(minElement);
+    }
+
+}
+
+string finalResiult = string.Empty;
+while (stack.Any())
+{ 
+    finalResiult += stack.Pop() + ", ";
+}
+
+Console.WriteLine(finalResiult.TrimEnd(',',' '));
