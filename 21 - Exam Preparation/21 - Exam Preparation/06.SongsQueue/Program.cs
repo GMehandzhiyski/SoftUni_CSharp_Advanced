a3f829373fa6cc34166a68275c2cad756a06da38
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-02-09 11:35:14 +0200

    06.SongsQueue 100% done

--- 21 - Exam Preparation/21 - Exam Preparation/06.SongsQueue/Program.cs
+++ 21 - Exam Preparation/21 - Exam Preparation/06.SongsQueue/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using System.Security.Cryptography.X509Certificates;
+
+string[] inputSongs = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+Queue<string> songs = new Queue<string>(inputSongs);  
+
+while (songs.Any())
+{
+    string[] commands = Console.ReadLine()
+         .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+   string command = commands[0];
+
+    if (command == "Play")
+    {
+        songs.Dequeue();
+    }
+    else if (command == "Add")
+    {
+        string songName = string.Join(" ", commands[1..]);
+        bool isSongAvalivable = CheckSoung(songs, songName);
+        if (!isSongAvalivable)
+        {
+            songs.Enqueue(songName);
+        }
+        else
+        {
+            Console.WriteLine($"{songName} is already contained!");
+        }
+
+    }
+    else if (command == "Show") 
+    {
+        PrintAllSongs(songs);
+    }
+
+}
+
+void PrintAllSongs(Queue<string> songs)
+{
+    string finalSongs = string.Empty;
+    foreach (var currSong in songs)
+    {
+        finalSongs += currSong + ", "; 
+    }
+    Console.WriteLine(finalSongs.TrimEnd(',',' '));
+}
+
+bool CheckSoung(Queue<string> songs, string songName)
+{
+    bool currName;
+   return currName = songs.Contains(songName);
+}
+
+Console.WriteLine("No more songs!");
