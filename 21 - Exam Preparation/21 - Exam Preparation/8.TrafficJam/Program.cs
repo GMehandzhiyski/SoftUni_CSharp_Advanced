1505405637695b6e329248a1c244b268724f6258
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-02-09 06:03:32 +0200

    8.TrafficJam 100% done

---
+++ 21 - Exam Preparation/21 - Exam Preparation/8.TrafficJam/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Queue<string> queueTrafic = new();   
+
+int n = int.Parse(Console.ReadLine());
+string arguments = string.Empty;
+int passedCouter = 0;
+while ((arguments = Console.ReadLine()) != "end")
+{
+    if (arguments == "green")
+    {
+        for (int i = 0; i < n && queueTrafic.Any(); i++)
+        {
+            Console.WriteLine($"{queueTrafic.Dequeue()} passed!");
+            passedCouter++;
+        }
+
+        continue;
+    }
+    
+    queueTrafic.Enqueue(arguments);
+}
+
+Console.WriteLine($"{passedCouter} cars passed the crossroads.");
