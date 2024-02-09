5b759f5bbf5924d701a7f19f1589d78e63df55d0
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-02-09 06:19:54 +0200

    01.BasicStackOperations 100% done

--- 21 - Exam Preparation/21 - Exam Preparation/01.BasicStackOperations/Program.cs
+++ 21 - Exam Preparation/21 - Exam Preparation/01.BasicStackOperations/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+int[] numbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+int nPush = numbers[0];
+int nPop = numbers[1];
+int xCheck = numbers[2];
+
+int[] secondNumbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+Stack<int> stack = new Stack<int>();    
+for (int i = 0; i < nPush; i++)
+{
+    stack.Push(secondNumbers[i]);
+}
+
+for (int i = 0; i < nPop; i++)
+{
+    stack.Pop();
+}
+
+int smalNumber = int.MaxValue;
+
+if (stack.Count == 0)
+{
+    Console.WriteLine(0);
+    return;
+}
+while (stack.Any())
+{
+    int currNumber  = stack.Pop();
+
+    if (currNumber == xCheck)
+    {
+        Console.WriteLine("true");
+        return;
+    }
+    if (currNumber < smalNumber)
+    {
+        smalNumber = currNumber;
+       
+    }
+}
+Console.WriteLine(smalNumber);
