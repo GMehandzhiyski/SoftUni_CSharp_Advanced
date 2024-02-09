8abafa616a77a00fc7728e5974f7ac1424d7c962
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-02-09 06:23:00 +0200

    02.BasicQueueOperations 100% done

---
+++ 21 - Exam Preparation/21 - Exam Preparation/02.BasicQueueOperations/Program.cs
@@ -0,0 +1,51 @@
+﻿
+
+
+int[] numbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+int nPush = numbers[0];
+int nPop = numbers[1];
+int xCheck = numbers[2];
+
+int[] secondNumbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+Queue<int> queue = new();
+for (int i = 0; i < nPush; i++)
+{
+    queue.Enqueue(secondNumbers[i]);
+}
+
+for (int i = 0; i < nPop; i++)
+{
+    queue.Dequeue();
+}
+
+int smalNumber = int.MaxValue;
+
+if (queue.Count == 0)
+{
+    Console.WriteLine(0);
+    return;
+}
+while (queue.Any())
+{
+    int currNumber = queue.Dequeue();
+
+    if (currNumber == xCheck)
+    {
+        Console.WriteLine("true");
+        return;
+    }
+    if (currNumber < smalNumber)
+    {
+        smalNumber = currNumber;
+
+    }
+}
+Console.WriteLine(smalNumber);
