db6e3b6e75740ef856c7301caa83991107679107
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-02-09 10:11:01 +0200

    04.FastFood 100% done

---
+++ 21 - Exam Preparation/21 - Exam Preparation/04.FastFood/Program.cs
@@ -0,0 +1,39 @@
+﻿
+int foodPerDay = int.Parse(Console.ReadLine());
+
+int[] inputOrdes = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+Queue<int> orders = new(inputOrdes);
+
+Console.WriteLine(orders.Max());
+if (orders.Sum() <= foodPerDay)
+{
+    Console.WriteLine("Order complete");
+    return;
+}
+
+
+while (true)
+{
+    int currOrder = orders.Peek();
+    if (currOrder < foodPerDay)
+    {
+        orders.Dequeue();
+        foodPerDay -= currOrder;
+    }
+    else
+    {
+        break;
+    }
+}
+
+
+string orderLeft = string.Empty;
+while (orders.Any())
+{
+    orderLeft += orders.Dequeue() + " ";
+}
+Console.WriteLine($"Order left: {orderLeft.TrimEnd(' ')}");
