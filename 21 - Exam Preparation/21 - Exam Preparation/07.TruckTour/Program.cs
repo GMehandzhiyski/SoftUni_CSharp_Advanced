75528ef0eab4cbf89cb7c13fc4ffa04c42096d65
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-02-10 06:42:11 +0200

    08.BalancedParenthesis 100% done

--- 21 - Exam Preparation/21 - Exam Preparation/07.TruckTour/Program.cs
+++ 21 - Exam Preparation/21 - Exam Preparation/07.TruckTour/Program.cs
@@ -1,2 +1,90 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//using System.ComponentModel.Design;
+
+//int numberPump = int.Parse(Console.ReadLine());
+//Queue<(int, int)> pumpes = new();
+
+//for (int i = 0; i < numberPump; i++)
+//{
+//    string[] inputPumps = Console.ReadLine()
+//        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+//    pumpes.Enqueue((int.Parse(inputPumps[0]), int.Parse(inputPumps[1])));
+//}
+//int counterPump = 0;
+//while (true) 
+//{
+//    foreach ((int,int) pump in pumpes)
+//    {
+//        if (pump.Item1 < pump.Item2)
+//        {
+//            counterPump++;
+//        }
+//        else 
+//        {
+//            Console.WriteLine(counterPump);
+//            return;
+//        }
+
+//    }
+//}
+
+namespace _07._Truck_Tour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // int number of pump
+            // petrol(L) distance(km)
+
+            int numberOfPump = int.Parse(Console.ReadLine());
+
+            Queue<(int, int)> pumps = new Queue<(int, int)>();
+
+            for (int i = 0; i < numberOfPump; i++)
+            {
+                int[] pumpsInput = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+
+
+                int petrol = pumpsInput[0];
+                int distance = pumpsInput[1];
+
+                pumps.Enqueue((petrol, distance));
+            }
+            int bestRoute = 0;
+            while (true)
+            {
+                int petrol = 0;
+                foreach ((int, int) pump in pumps)
+                {
+                    petrol += pump.Item1;
+                    int distance = pump.Item2;
+                    if (petrol - distance < 0)
+                    {
+                        petrol = -1;
+                        break;
+                    }
+                    else
+                    {
+                        petrol -= distance;
+                    }
+                }
+                if (petrol >= 0)
+                {
+                    break;
+                }
+                bestRoute++;
+                pumps.Enqueue(pumps.Dequeue());
+
+
+            }
+
+            Console.WriteLine(bestRoute);
+        }
+    }
+}
---
+++ 21 - Exam Preparation/21 - Exam Preparation/08.BalancedParenthesis/Program.cs
@@ -0,0 +1,64 @@
+﻿
+string inputParent = Console.ReadLine();
+
+Stack<char> holderPatent = new();
+
+
+while (true)
+{
+   foreach (var newSymbol in inputParent)
+    {
+       // string newSymbol = ;
+        bool isCorrectSymbol = CheckSymbol(newSymbol);
+        if (isCorrectSymbol
+            || holderPatent.Count == 0)
+        {
+            holderPatent.Push(newSymbol);
+        }
+        else 
+        {
+            char currSymbol = holderPatent.Peek();
+
+            bool isNewSymboIsCorrect = CheckNewSymbol(currSymbol, newSymbol);
+
+            if (isNewSymboIsCorrect)
+            {
+                holderPatent.Pop();
+            }
+            //else
+            //{
+            //    Console.WriteLine("NO");
+            //    return;
+            //}
+        }
+    }
+
+    if (holderPatent.Count == 0)
+    {
+        Console.WriteLine("YES");
+    }
+    else 
+    {
+        Console.WriteLine("NO");
+    }
+   
+    return;
+}
+
+bool CheckSymbol(char newSymbol)
+{
+    return (newSymbol == '(')
+            || (newSymbol == '{')
+            || (newSymbol == '[');
+}
+
+bool CheckNewSymbol(char currSymbol, char newSymbol)
+{
+    return (currSymbol == '('
+            && newSymbol == ')')
+     || (currSymbol == '{'
+            && newSymbol == '}')
+     || (currSymbol == '['
+            && newSymbol == ']');
+
+}
