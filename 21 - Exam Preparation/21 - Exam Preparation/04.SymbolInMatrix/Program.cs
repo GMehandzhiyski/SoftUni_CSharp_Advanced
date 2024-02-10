9d84ca1ea1a6276e72105f3a9daa76fa43918bea
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-02-10 07:52:11 +0200

    04.SymbolInMatrix 100% done

---
+++ 21 - Exam Preparation/21 - Exam Preparation/04.SymbolInMatrix/Program.cs
@@ -0,0 +1,43 @@
+﻿
+int matrixSize = int.Parse(Console.ReadLine());
+
+int rows = matrixSize;
+int cols = matrixSize;
+
+string[,] matrix = new string[rows, cols];
+
+
+
+for (int i = 0; i < rows; i++)
+{
+    string inputString = Console.ReadLine();
+    string[] inputStingArray = new string[matrixSize];
+    int counter = 0;
+
+    foreach (var item in inputString)
+    {
+        inputStingArray[counter] = item.ToString();
+        counter++;
+    }
+
+    for (int j = 0; j < cols; j++)
+    { 
+            matrix[i,j] = inputStingArray[j];
+    }
+    
+}
+
+string searchSymbol = Console.ReadLine();
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        if (searchSymbol == matrix[i, j])
+        {
+            Console.WriteLine($"({i}, {j})");
+            return;
+        }
+    }
+}
+Console.WriteLine($"{searchSymbol} does not occur in the matrix");
