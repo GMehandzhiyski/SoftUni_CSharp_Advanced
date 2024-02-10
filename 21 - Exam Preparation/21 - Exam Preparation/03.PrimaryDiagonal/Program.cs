fbaf0dba54117382c17fd6e2f5b6482681180d46
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-02-10 07:25:34 +0200

    03.PrimaryDiagonal  100% done

---
+++ 21 - Exam Preparation/21 - Exam Preparation/03.PrimaryDiagonal/Program.cs
@@ -0,0 +1,27 @@
+﻿
+int matrixSize = int.Parse(Console.ReadLine());
+
+int cols = matrixSize;
+int rols = matrixSize;
+
+int[,] matrix = new int[cols, rols];
+
+for (int i = 0; i < cols; i++)
+{
+    int[] inputValue = Console.ReadLine()
+           .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+    for (int j = 0; j < rols; j++)
+    {
+        matrix[i, j] = inputValue[j];
+    }
+}
+int sumPrimariDiagonal = 0;
+for (int i = 0; i < cols; i++)
+{
+    sumPrimariDiagonal += matrix[i, i];
+}
+
+Console.WriteLine(sumPrimariDiagonal);
