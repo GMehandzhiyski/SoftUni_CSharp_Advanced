ba872516ea0795be321359992709fee23fa6e0ec
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-02-12 21:12:08 +0200

    01.WormsAndHoles 100% done

--- 21 - Exam Preparation/21 - Exam Preparation/01.WormsAndHoles/Program.cs
+++ 21 - Exam Preparation/21 - Exam Preparation/01.WormsAndHoles/Program.cs
@@ -1,10 +1,105 @@
-﻿namespace _01.WormsAndHoles
+﻿using System.Text;
+
+namespace _01.WormsAndHoles
 {
-    internal class Program
+   public class Program
     {
         static void Main(string[] args)
         {
+
+
             
+            int[] inputWorms =  Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] inputHoles = Console.ReadLine()
+              .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+              .Select(int.Parse)
+              .ToArray();
+
+            Stack<int> worms = new Stack<int>(inputWorms);
+            Queue<int> holes = new Queue<int>(inputHoles);
+
+            int matches = 0;
+            int noneWorms = 0;
+            bool wormsNotFit = false;
+
+            while (worms.Count > 0
+                && holes.Count > 0) 
+            {
+              int  currWormValue = worms.Pop();
+              int currHoleValue = holes.Dequeue();
+
+                if (currWormValue == currHoleValue)
+                {
+                    matches++;
+
+                }
+                else 
+                {   
+                    wormsNotFit |= true;
+                    currWormValue -= 3;
+                    if (currWormValue > 0) 
+                    {
+                        worms.Push(currWormValue);
+                    }
+                    
+                }
+            
+            
+            }
+
+            // first line
+            if (matches > 0)
+            {
+                Console.WriteLine($"Matches: {matches}");
+            }
+            else
+            {
+                Console.WriteLine("There are no matches.");
+            }
+
+            // second line
+            if (worms.Count == 0)
+            {
+                Console.WriteLine("Every worm found a suitable hole!");
+
+            }
+            else if (wormsNotFit
+                    && worms.Count == 0)
+            { 
+            
+            }
+            else
+            {
+                StringBuilder sb = new StringBuilder();
+
+                foreach (var worm in worms)
+                {
+                    sb.Append(worm + ", ");
+                }
+                Console.WriteLine($"Worms left: {sb.ToString().TrimEnd(',', ' ')}");
+            }
+
+            // third line
+
+            if (!holes.Any())
+            {
+                Console.WriteLine("Holes left: none");
+            }
+            else
+            {
+                StringBuilder sb = new StringBuilder();
+
+                foreach (var hole in holes)
+                {
+                    sb.Append(hole + ", ");
+                }
+                Console.WriteLine($"Holes left: {sb.ToString().TrimEnd(',', ' ')}");
+            }
+
         }
     }
 }
