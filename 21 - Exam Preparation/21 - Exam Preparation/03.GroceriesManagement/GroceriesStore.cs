c8da4f5a16577de57b675da2979d25ed3c1e6aa5
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-02-13 21:31:45 +0200

    03.GroceriesManagement create

---
+++ 21 - Exam Preparation/21 - Exam Preparation/03.GroceriesManagement/GroceriesStore.cs
@@ -0,0 +1,51 @@
+﻿namespace GroceriesManagement
+{
+    public class GroceriesStore
+    {
+		private int capacity;
+		private double turnover;
+		private List<Product> stall;
+
+        public GroceriesStore(int capacity)
+        {
+            Capacity = capacity;
+            Turnover = 0;
+            Stall = new List<Product>();
+        }
+
+        public List<Product> Stall
+		{
+			get { return stall; }
+			set { stall = value; }
+		}
+
+
+		public double Turnover
+		{
+			get { return turnover; }
+			set { turnover = value; }
+		}
+
+
+		public int Capacity
+		{
+			get { return capacity; }
+			set { capacity = value; }
+		}
+
+
+        public void AddProduct(Product product)
+        {
+			if (!Stall.Contains(product) && Stall.Count > Capacity)
+			{ 
+			 Stall.Add(product);
+			}
+        }
+
+		public void RemoveProduct(Product product) 
+		{
+		
+		}
+
+    }
+}
---
+++ 21 - Exam Preparation/21 - Exam Preparation/03.GroceriesManagement/Product.cs
@@ -0,0 +1,34 @@
+﻿namespace GroceriesManagement
+{
+    public class Product
+    {
+		private string name;
+		private double price;
+
+        public Product(string name, double price)
+        {
+			Name = name;
+			Price = price;
+        }
+
+        public double Price
+		{
+			get { return price; }
+			set { price = value; }
+		}
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		public override string ToString() 
+		{
+			return $"{Name}: {Price:F2}";
+		}
+
+
+
+    }
+}
