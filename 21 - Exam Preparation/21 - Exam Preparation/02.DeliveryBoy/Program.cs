b2de1710635c4c39459cfdc0d114887722476dd7
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-02-15 12:30:59 +0200

    02.DeliveryBoy 100% done

--- 21 - Exam Preparation/21 - Exam Preparation/02.DeliveryBoy/Program.cs
+++ 21 - Exam Preparation/21 - Exam Preparation/02.DeliveryBoy/Program.cs
@@ -1,6 +1,26 @@
 ﻿using System.Runtime.CompilerServices;
 using System.Text;
+/*
+5 6
+B----A
+*-***-
+*-***-
+*----P
+******
+right
+down
+down
+down
+right
+right
+right
+right
+up
+up
+up
 
+
+*/
 namespace _02.DeliveryBoy
 {
     internal class Program
@@ -31,11 +51,12 @@ namespace _02.DeliveryBoy
                     {
                         firstPlayerRow = row;
                         firstPlayerCol = col;
+                        playerRow = row;
+                        playerCol = col;
                     }
                 }
             }
-            playerRow = firstPlayerRow;
-            playerCol = firstPlayerCol;
+            
             int currRow = 0;
             int currCol = 0;
             int nextRow = 0;
@@ -47,53 +68,314 @@ namespace _02.DeliveryBoy
                 string token = Console.ReadLine();
 
                 if (token == "up")
-                { 
-                
+                {
+                    currCol = playerCol;
+                    currRow = playerRow;
+                    nextRow = currRow - 1;
+                    nextCol = currCol;
+
+                    bool isCoordinateIsValid = CheckCoordinate(field, nextRow, nextCol);
+                    if (!isCoordinateIsValid)
+                    {
+                        if (field[currRow, currCol] == '-'
+                              || field[currRow, currCol] == 'B')
+                        {
+                            field[currRow, currCol] = '.';
+                        }
+                        Console.WriteLine("The delivery is late. Order is canceled.");
+                        field[firstPlayerRow, firstPlayerCol] =' ';
+                        PrintField(field);
+                        return;
+                    }
+
+                    if (field[nextRow, nextCol] == '-')
+                    {
+                        if (field[currRow, currCol] == '-'
+                            || field[currRow, currCol] == 'B')
+                        {
+                            field[currRow, currCol] = '.';
+                        }
+                    }
+                    else if (field[nextRow, nextCol] == 'P')
+                    {
+                        if (field[currRow, currCol] == '-'
+                            || field[currRow, currCol] == 'B')
+                        {
+                            field[currRow, currCol] = '.';
+                        }
+                        field[nextRow, nextCol] = 'R';
+                        hetakePizza = true;
+                        Console.WriteLine("Pizza is collected. 10 minutes for delivery.");
+
+                    }
+                    else if (field[nextRow, nextCol] == 'A'
+                             && hetakePizza)
+                    {
+                        if (field[currRow, currCol] == '-'
+                            || field[currRow, currCol] == 'B')
+                        {
+                            field[currRow, currCol] = '.';
+                        }
+                        field[nextRow, nextCol] = 'P';
+                        field[firstPlayerRow, firstPlayerCol] = 'B';
+                        Console.WriteLine("Pizza is delivered on time! Next order...");
+                        PrintField(field);
+                        return;
+                    }
+                    else if (field[nextRow, nextCol] == 'A'
+                             && !hetakePizza)
+                    {
+                        if (field[currRow, currCol] == '-'
+                                 || field[currRow, currCol] == 'B')
+                        {
+                            field[currRow, currCol] = '.';
+                        }
+
+                    }
+                    else if (field[nextRow, nextCol] == '*')
+                    {
+                        playerRow = currRow;
+                        playerCol = currCol;
+                        continue;
+                    }
+
+                    playerRow = nextRow;
+                    playerCol = nextCol;
+                    //PrintField(field);
                 }
                 else if (token == "down")
                 {
                     currCol = playerCol;
                     currRow = playerRow;
                     nextRow = currRow + 1 ;
-                    nextCol = currRow;
+                    nextCol = currCol;
 
                     bool isCoordinateIsValid = CheckCoordinate(field, nextRow, nextCol);
                     if (!isCoordinateIsValid) 
                     {
+                        if (field[currRow, currCol] == '-'
+                              || field[currRow, currCol] == 'B')
+                        {
+                            field[currRow, currCol] = '.';
+                        }
                         Console.WriteLine("The delivery is late. Order is canceled.");
-                        field[playerRow, playerCol] = ' ';
+                        field[firstPlayerRow, firstPlayerCol] = ' ';
                         PrintField(field);
+                        return;
                     }
 
-                    if (field[nextRow, nextCol] == 'P')
+                    if (field[nextRow, nextCol] == '-')
                     {
+                        if (field[currRow, currCol] == '-'
+                              || field[currRow, currCol] == 'B')
+                        {
+                            field[currRow, currCol] = '.';
+                        }
+                    }
+                    else if (field[nextRow, nextCol] == 'P')
+                    {
+                        if (field[currRow, currCol] == '-'
+                             || field[currRow, currCol] == 'B')
+                        {
+                            field[currRow, currCol] = '.';
+                        }
                         field[nextRow, nextCol] = 'R';
                         hetakePizza = true;
+                        Console.WriteLine("Pizza is collected. 10 minutes for delivery.");
 
                     }
                     else if (field[nextRow, nextCol] == 'A'
                              && hetakePizza)
                     {
+                        if (field[currRow, currCol] == '-'
+                            || field[currRow, currCol] == 'B')
+                        {
+                            field[currRow, currCol] = '.';
+                        }
                         field[nextRow, nextCol] = 'P';
                         field[firstPlayerRow, firstPlayerCol] = 'B';
+                        Console.WriteLine("Pizza is delivered on time! Next order...");
+                        PrintField(field);
+                        return;
                     }
-                    else if (field[nextRow, nextCol] == '-')
+                    else if (field[nextRow, nextCol] == 'A'
+                             && !hetakePizza)
                     {
-                        field[nextRow, nextCol] = '.';
+                        if (field[currRow, currCol] == '-'
+                                 || field[currRow, currCol] == 'B')
+                        {
+                            field[currRow, currCol] = '.';
+                        }
+
                     }
-                    else
+                    else if(field[nextRow, nextCol] == '*')
                     {
+                        playerRow = currRow;
+                        playerCol = currCol;
                         continue;
                     }
+
+                    playerRow = nextRow;
+                    playerCol = nextCol;
+                    //PrintField (field);
 
                 }
                 else if (token == "right")
                 {
+                    currCol = playerCol;
+                    currRow = playerRow;
+                    nextRow = currRow;
+                    nextCol = currCol + 1; 
 
+                    bool isCoordinateIsValid = CheckCoordinate(field, nextRow, nextCol);
+                    if (!isCoordinateIsValid)
+                    {
+                        if (field[currRow, currCol] == '-'
+                              || field[currRow, currCol] == 'B')
+                        {
+                            field[currRow, currCol] = '.';
+                        }
+                        Console.WriteLine("The delivery is late. Order is canceled.");
+                        field[firstPlayerRow, firstPlayerCol] = ' ';
+                        PrintField(field);
+                        return;
+                    }
+
+                    if (field[nextRow, nextCol] == '-')
+                    {
+                        if (field[currRow, currCol] == '-'
+                            || field[currRow, currCol] == 'B')
+                        {
+                            field[currRow, currCol] = '.';
+                        }
+
+                    }
+                    else if (field[nextRow, nextCol] == 'P')
+                    {
+                        if (field[currRow, currCol] == '-'
+                             || field[currRow, currCol] == 'B')
+                        {
+                            field[currRow, currCol] = '.';
+                        }
+                        field[nextRow, nextCol] = 'R';
+                        hetakePizza = true;
+                        Console.WriteLine("Pizza is collected. 10 minutes for delivery.");
+
+                    }
+                    else if (field[nextRow, nextCol] == 'A'
+                             && hetakePizza)
+                    {
+                        if (field[currRow, currCol] == '-'
+                            || field[currRow, currCol] == 'B')
+                        {
+                            field[currRow, currCol] = '.';
+                        }
+                        field[nextRow, nextCol] = 'P';
+                        field[firstPlayerRow, firstPlayerCol] = 'B';
+                        Console.WriteLine("Pizza is delivered on time! Next order...");
+                        PrintField(field);
+                        return;
+                    }
+                    else if (field[nextRow, nextCol] == 'A'
+                             && !hetakePizza)
+                    {
+                        if (field[currRow, currCol] == '-'
+                                 || field[currRow, currCol] == 'B')
+                        {
+                            field[currRow, currCol] = '.';
+                        }
+
+                    }
+                    else if (field[nextRow, nextCol] == '*')
+                    {
+                        playerRow = currRow;
+                        playerCol = currCol;
+                        continue;
+                    }
+
+                    playerRow = nextRow;
+                    playerCol = nextCol;
+                    //PrintField(field);
                 }
                 else if (token == "left")
                 {
+                    currCol = playerCol;
+                    currRow = playerRow;
+                    nextRow = currRow;
+                    nextCol = currCol - 1;
 
+                    bool isCoordinateIsValid = CheckCoordinate(field, nextRow, nextCol);
+                    if (!isCoordinateIsValid)
+                    {
+                        if (field[currRow, currCol] == '-'
+                              || field[currRow, currCol] == 'B')
+                        {
+                            field[currRow, currCol] = '.';
+                        }
+                        Console.WriteLine("The delivery is late. Order is canceled.");
+                        field[firstPlayerRow, firstPlayerCol] = ' ';
+                        PrintField(field);
+                        return;
+                    }
+
+                    if (field[nextRow, nextCol] == '-')
+                    {
+                        if (field[currRow, currCol] == '-'
+                             || field[currRow, currCol] == 'B')
+                        {
+                            field[currRow, currCol] = '.';
+                        }
+
+                    }
+                    else if (field[nextRow, nextCol] == 'P')
+                    {
+                        if (field[currRow, currCol] == '-'
+                            || field[currRow, currCol] == 'B')
+                        {
+                            field[currRow, currCol] = '.';
+                        }
+                        field[nextRow, nextCol] = 'R';
+                        hetakePizza = true;
+                        Console.WriteLine("Pizza is collected. 10 minutes for delivery.");
+
+                    }
+                    else if (field[nextRow, nextCol] == 'A'
+                             && hetakePizza)
+                    {
+                        if (field[currRow, currCol] == '-'
+                             || field[currRow, currCol] == 'B')
+                        {
+                            field[currRow, currCol] = '.';
+                        }
+                        field[nextRow, nextCol] = 'P';
+                        field[firstPlayerRow, firstPlayerCol] = 'B';
+                        Console.WriteLine("Pizza is delivered on time! Next order...");
+                        PrintField(field);
+                        return;
+                    }
+                    else if (field[nextRow, nextCol] == 'A'
+                             && !hetakePizza)
+                    {
+                        if (field[currRow, currCol] == '-'
+                                 || field[currRow, currCol] == 'B')
+                        {
+                            field[currRow, currCol] = '.';
+                        }
+
+                    }
+                    else if (field[nextRow, nextCol] == '*')
+                    {
+                        playerRow = currRow;
+                        playerCol = currCol;
+                        //PrintField(field);
+                        continue;
+
+                    }
+                   
+
+                    playerRow = nextRow;
+                    playerCol = nextCol;
+                    //PrintField(field);
                 }
 
 
@@ -102,9 +384,10 @@ namespace _02.DeliveryBoy
 
         private static void PrintField(char[,] field)
         {
-            StringBuilder   sb = new StringBuilder();   
+               
             for (int row = 0; row < field.GetLength(0); row++)
             {
+                StringBuilder sb = new StringBuilder();
                 for (int col = 0; col < field.GetLength(1); col++)
                 {
                     sb.Append(field[row, col]);
--- 21 - Exam Preparation/21 - Exam Preparation/aasas/Program.cs
+++ 21 - Exam Preparation/21 - Exam Preparation/aasas/Program.cs
@@ -1,13 +1,15 @@
 ﻿namespace _01.MonsterExtermination
 {
-   public class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+
+
             int[] monstarArmorInput = Console.ReadLine()
-                .Split(",", StringSplitOptions.RemoveEmptyEntries)
-                .Select(int.Parse)
-                .ToArray();
+              .Split(",", StringSplitOptions.RemoveEmptyEntries)
+              .Select(int.Parse)
+              .ToArray();
             int[] soldierImpactInput = Console.ReadLine()
                 .Split(",", StringSplitOptions.RemoveEmptyEntries)
                 .Select(int.Parse)
@@ -39,33 +41,104 @@
                             nextSoldierImpact = soldierImpact.Pop();
                         }
                         soldierImpact.Push(currSoldierImpactRemainder + nextSoldierImpact);
-                       
+
                     }
 
                     killedMonsters++;
                 }
                 if (currSoldierImpact < currMonsterArmor)
                 {
+//==================================================================================
                     currMonsterArmorRemainder = currMonsterArmor - originalStrike;
-                    if (currMonsterArmorRemainder > 0)
-                    {
-                        monstarArmor.Enqueue(currMonsterArmorRemainder);
-                    }
-                    
+                    monstarArmor.Enqueue(currMonsterArmorRemainder);
+ //=======================================================================================                  
+
                 }
             }
-            if (monstarArmor.Any()
-                && !soldierImpact.Any())
-            {
-                
-                Console.WriteLine("The soldier has been defeated.");
-            }
-            else
+//==================================================================================================
+            if (monstarArmor.Count == 0)
             {
                 Console.WriteLine("All monsters have been killed!");
             }
 
+            if(soldierImpact.Count == 0)
+            {
+                Console.WriteLine("The soldier has been defeated.");
+            }
+
             Console.WriteLine($"Total monsters killed: {killedMonsters}");
+//======================================================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //int[] monstarArmorInput = Console.ReadLine()
+            //    .Split(",", StringSplitOptions.RemoveEmptyEntries)
+            //    .Select(int.Parse)
+            //    .ToArray();
+            //int[] soldierImpactInput = Console.ReadLine()
+            //    .Split(",", StringSplitOptions.RemoveEmptyEntries)
+            //    .Select(int.Parse)
+            //    .ToArray();
+
+            //Queue<int> monstarArmor = new Queue<int>(monstarArmorInput);
+            //Stack<int> soldierImpact = new Stack<int>(soldierImpactInput);
+
+            //int killedMonsters = 0;
+
+            //while (monstarArmor.Any()
+            //       && soldierImpact.Any())
+            //{
+            //    int currMonsterArmor = monstarArmor.Dequeue();
+            //    int currSoldierImpact = soldierImpact.Pop();
+            //    int originalStrike = currSoldierImpact;
+            //    int nextSoldierImpact = 0;
+            //    int currSoldierImpactRemainder = 0;
+            //    int currMonsterArmorRemainder = 0;
+
+            //    if (currSoldierImpact >= currMonsterArmor)
+            //    {
+            //        currSoldierImpactRemainder = currSoldierImpact - currMonsterArmor;
+            //        if (currSoldierImpactRemainder > 0)
+            //        {
+            //            if (soldierImpact.Any())
+            //            {
+            //                nextSoldierImpact = soldierImpact.Pop();
+            //            }
+
+            //            soldierImpact.Push(currSoldierImpactRemainder + nextSoldierImpact);
+            //        }
+
+            //        killedMonsters++;
+            //    }
+            //    else 
+            //    {
+            //        currMonsterArmorRemainder = currMonsterArmor - originalStrike;
+            //        monstarArmor.Enqueue(currMonsterArmorRemainder);
+            //    }
+            //}
+            //if (monstarArmor.Count == 0)
+            //{
+            //    Console.WriteLine("All monsters have been killed!");
+            //}
+
+            //if(soldierImpact.Count == 0)
+            //{
+            //    Console.WriteLine("The soldier has been defeated.");
+            //}
+
+            //Console.WriteLine($"Total monsters killed: {killedMonsters}");
         }
     }
 }
