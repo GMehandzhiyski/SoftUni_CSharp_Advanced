a69ce26260c37ffe04d8548aa6e8c989c3f568d6
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-02-15 16:25:47 +0200

    01.RubberDuckDebugers 100% done

--- 21 - Exam Preparation/21 - Exam Preparation/01.RubberDuckDebugers/Program.cs
+++ 21 - Exam Preparation/21 - Exam Preparation/01.RubberDuckDebugers/Program.cs
@@ -4,7 +4,63 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] timeInput = Console.ReadLine()
+             .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+             .Select(int.Parse)
+             .ToArray();
+            int[] taskInput = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> times = new Queue<int>(timeInput);
+            Stack<int> tasks = new Stack<int>(taskInput);
+
+            int darthVaderDucky = 0;
+            int thorDucky = 0;
+            int bigBlueRubberDucky = 0;
+            int smallYellowRubberDucky = 0;
+
+            while (times.Any()
+                   && tasks.Any())
+            {
+                int currTime = times.Dequeue();
+                int currTask = tasks.Pop();
+                int multiplaySum = currTask * currTime;
+                
+
+
+
+                if (multiplaySum >= 0 && 60 >= multiplaySum)
+                {
+                    darthVaderDucky++;
+                }
+                else if (multiplaySum >= 61 && 120 >= multiplaySum)
+                {
+                    thorDucky++;
+                }
+                else if (multiplaySum >= 121 && 180 >= multiplaySum)
+                {
+                    bigBlueRubberDucky++;
+                }
+                else if (multiplaySum >= 181 && 240 >= multiplaySum)
+                {
+                    smallYellowRubberDucky++;
+                }
+                else 
+                {
+                    currTask -= 2;
+                    tasks.Push(currTask);
+                    times.Enqueue(currTime);
+                }
+            }
+
+            Console.WriteLine("Congratulations, all tasks have been completed! Rubber ducks rewarded:");
+            Console.WriteLine($"Darth Vader Ducky: {darthVaderDucky}");
+            Console.WriteLine($"Thor Ducky: {thorDucky}");
+            Console.WriteLine($"Big Blue Rubber Ducky: {bigBlueRubberDucky}");
+            Console.WriteLine($"Small Yellow Rubber Ducky: {smallYellowRubberDucky}");
+
         }
     }
 }
--- 21 - Exam Preparation/21 - Exam Preparation/03.AutomotiveRepairShop/RepairShop.cs
+++ 21 - Exam Preparation/21 - Exam Preparation/03.AutomotiveRepairShop/RepairShop.cs
@@ -1,110 +1,110 @@
-﻿using System.Text;
-
-namespace AutomotiveRepairShop
-{
-    public class RepairShop
-    {
-        public RepairShop(int capacity)
-        {
-            Capacity = capacity;
-            this.Vehicles = new List<Vehicle>();
-        }
-
-        public int Capacity { get; set; }
-
-        public List<Vehicle> Vehicles { get; set; }
-
-        public void AddVehicle(Vehicle vehicle)
-        {
-            if (this.Vehicles.Count < this.Capacity)
-            {
-                this.Vehicles.Add(vehicle);
-            }
-        }
-
-        public bool RemoveVehicle(string vin) => this.Vehicles.Remove(this.Vehicles.FirstOrDefault(v => v.VIN == vin));
-
-        public int GetCount() => this.Vehicles.Count;
-
-        public Vehicle GetLowestMileage() => this.Vehicles.OrderBy(v => v.Mileage).FirstOrDefault();
-
-        public string Report()
-        {
-            StringBuilder sb = new StringBuilder();
-            sb.AppendLine("Vehicles in the preparatory:");
-
-            foreach (Vehicle v in this.Vehicles)
-            {
-                sb.AppendLine(v.ToString());
-            }
-            return sb.ToString().TrimEnd();
-        }
-    }
-}
-
-
-//using System.Text;
+﻿//using System.Text;
 
 //namespace AutomotiveRepairShop
 //{
 //    public class RepairShop
 //    {
-//		private int capacity;
-//		private List<Vehicle> vehicles;
-
 //        public RepairShop(int capacity)
 //        {
 //            Capacity = capacity;
-//            Vehicles = new List<Vehicle>();
+//            this.Vehicles = new List<Vehicle>();
 //        }
 
-//        public int Capacity
-//		{
-//			get { return capacity; }
-//			set { capacity = value; }
-//		}
+//        public int Capacity { get; set; }
 
-//        public List<Vehicle> Vehicles
-//        {
-//			get { return vehicles; }
-//			set { vehicles = value; }
-//		}
+//        public List<Vehicle> Vehicles { get; set; }
 
-//        public void AddVehicle(Vehicle newVehicle)
+//        public void AddVehicle(Vehicle vehicle)
 //        {
-//            if (Vehicles.Count < Capacity)
+//            if (this.Vehicles.Count < this.Capacity)
 //            {
-//                Vehicles.Add(newVehicle);
+//                this.Vehicles.Add(vehicle);
 //            }
 //        }
 
-//        public bool RemoveVehicle(string vinNumber) 
-//        {
-//            return Vehicles.Remove(Vehicles.FirstOrDefault(v => v.VIN == vinNumber));
+//        public bool RemoveVehicle(string vin) => this.Vehicles.Remove(this.Vehicles.FirstOrDefault(v => v.VIN == vin));
 
-//        }
+//        public int GetCount() => this.Vehicles.Count;
 
-//        public int GetCount()
-//        {
-//            return Vehicles.Count;
-//        }
-
-//        public Vehicle GetLowestMileage()
-//        {
-//            Vehicle mileAge = Vehicles.OrderBy(v => v.Mileage).FirstOrDefault();
-//            return mileAge;
-//        }
+//        public Vehicle GetLowestMileage() => this.Vehicles.OrderBy(v => v.Mileage).FirstOrDefault();
 
 //        public string Report()
 //        {
 //            StringBuilder sb = new StringBuilder();
 //            sb.AppendLine("Vehicles in the preparatory:");
-//            foreach (Vehicle vehicle in Vehicles)
-//            {
-//                sb.AppendLine(vehicle.ToString());
-//            }
 
-//           return sb.ToString().TrimEnd();
+//            foreach (Vehicle v in this.Vehicles)
+//            {
+//                sb.AppendLine(v.ToString());
+//            }
+//            return sb.ToString().TrimEnd();
 //        }
 //    }
 //}
+
+
+using System.Text;
+
+namespace AutomotiveRepairShop
+{
+    public class RepairShop
+    {
+        private int capacity;
+        private List<Vehicle> vehicles;
+
+        public RepairShop(int capacity)
+        {
+            Capacity = capacity;
+            Vehicles = new List<Vehicle>();
+        }
+
+        public int Capacity
+        {
+            get { return capacity; }
+            set { capacity = value; }
+        }
+
+        public List<Vehicle> Vehicles
+        {
+            get { return vehicles; }
+            set { vehicles = value; }
+        }
+
+        public void AddVehicle(Vehicle newVehicle)
+        {
+            if (Vehicles.Count < Capacity)
+            {
+                Vehicles.Add(newVehicle);
+            }
+        }
+
+        public bool RemoveVehicle(string vinNumber)
+        {
+            return Vehicles.Remove(Vehicles.FirstOrDefault(v => v.VIN == vinNumber));
+
+        }
+
+        public int GetCount()
+        {
+            return Vehicles.Count;
+        }
+
+        public Vehicle GetLowestMileage()
+        {
+            Vehicle mileAge = Vehicles.OrderBy(v => v.Mileage).FirstOrDefault();
+            return mileAge;
+        }
+   
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Vehicles in the preparatory:");
+            foreach (Vehicle vehicle in Vehicles)
+            {
+                sb.AppendLine(vehicle.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
