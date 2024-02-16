2fe0647c98e101105f6fa3d91de96354cf7bb3f1
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-02-16 07:15:15 +0200

    03.ClothesMagazine create

---
+++ 21 - Exam Preparation/21 - Exam Preparation/03.ClothesMagazine/Cloth.cs
@@ -0,0 +1,39 @@
+﻿namespace ClothesMagazine
+{
+    public class Cloth
+    {
+		private string color;
+		private int size;
+		private string type;
+
+        public Cloth(string color, int size, string type)
+        {
+            Color = color;
+            Size = size;
+            Type = type;
+        }
+
+        public string Color
+		{
+			get { return color; }
+			set { color = value; }
+		}
+		public int Size
+		{
+			get { return size; }
+			set { size = value; }
+		}
+		public string Type
+		{
+			get { return type; }
+			set { type = value; }
+		}
+
+		public override string ToString() 
+		{
+			return $"Product: {Type} with size {Size}, color {Color}";
+
+        }
+
+	}
+}
---
+++ 21 - Exam Preparation/21 - Exam Preparation/03.ClothesMagazine/Magazine.cs
@@ -0,0 +1,48 @@
+﻿namespace ClothesMagazine
+{
+    public class Magazine
+    {
+		private string type;
+		private int capacity;
+		private List<Cloth> clothes;
+
+        public Magazine(string type, int capacity)
+        {
+            Type = type;
+            Capacity = capacity;
+            Clothes = new List<Cloth>();
+        }
+
+        public string Type
+		{
+			get { return type; }
+			set { type = value; }
+		}
+		public int Capacity
+        {
+			get { return capacity; }
+			set { capacity = value; }
+		}
+		public List<Cloth> Clothes
+        {
+			get { return clothes; }
+			set { clothes = value; }
+		}
+
+		public void AddCloth(Cloth newCloth)
+		{
+            if (Clothes.Count < Capacity)
+            {
+                Clothes.Add(newCloth);
+            }
+        }
+
+        public bool RemoveCloth(string newColor)
+        {
+            return Clothes.Remove(Clothes.FirstOrDefault( c => c.Color == newColor));
+        }
+
+
+
+    }
+}
