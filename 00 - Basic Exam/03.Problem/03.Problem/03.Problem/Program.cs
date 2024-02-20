3d7401ef35eb0c0b1131c38a76526847afbcc639
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-02-20 07:36:26 +0200

    Basic Exam Problem 03 100%done

---
+++ 00 - Basic Exam/03.Problem/03.Problem/03.Problem/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace _03.Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+
+            double totalPrice = 0.0;
+            double finalPrice = 0.0;
+
+            if (season == "spring")
+            {
+                if (people <= 5)
+                {
+                    finalPrice = people * 50;
+                }
+
+                else
+                {
+                    finalPrice = people * 48;
+                }
+
+
+            }
+
+            else if (season == "summer")
+            {
+                if (people <= 5)
+                {
+                    totalPrice = people * 48.50;
+                    finalPrice = totalPrice - (totalPrice  * (15.0 / 100.0));
+                }
+
+                else
+                {
+                    totalPrice = people * 45;
+                    finalPrice = totalPrice - (totalPrice  * (15.0 / 100.0));
+                }
+            }
+
+            else if (season == "autumn")
+            {
+                if (people <= 5)
+                {
+                    finalPrice = people * 60;
+                }
+
+                else
+                {
+                    finalPrice = people * 49.50;
+                }
+            }
+
+            else if (season == "winter")
+            {
+                if (people <= 5)
+                {
+                    totalPrice = people * 86.00;
+                    finalPrice = totalPrice + (totalPrice * (8.0 / 100.0));
+                }
+
+                else
+                {
+                    totalPrice = people * 85.00;
+                    finalPrice = totalPrice + (totalPrice * (8.0 / 100.0));
+                }
+            }
+
+            Console.WriteLine($"{finalPrice:f2} leva.");
+        }
+    }
+}
