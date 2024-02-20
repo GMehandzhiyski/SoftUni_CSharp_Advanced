ce2ae0d4653b04afe48d377cca3c9c033ee0abc2
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-02-20 07:37:00 +0200

    Basic Exam Problem 05 100%done

---
+++ 00 - Basic Exam/05.Problem/05.Problem/05.Problem/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _05.Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,int> players = new Dictionary<string,int>();  
+            string token = string.Empty; 
+            while ((token = Console.ReadLine()) != "END")
+            {
+                int goals = int.Parse(Console.ReadLine());
+                if (!players.ContainsKey(token))
+                {
+                    players.Add(token, goals);
+                }
+                if (goals >= 10)
+                {
+                    break;
+                }
+            }
+
+            var currPlayer = players.OrderByDescending(p => p.Value).FirstOrDefault();
+
+            Console.WriteLine($"{currPlayer.Key} is the best player!");
+            if (currPlayer.Value > 2)
+            {
+                Console.WriteLine($"He has scored {currPlayer.Value} goals and made a hat-trick !!!");
+            }
+            else
+            {
+                Console.WriteLine($"He has scored {currPlayer.Value} goals.");
+            }
+        }
+    }
+}
