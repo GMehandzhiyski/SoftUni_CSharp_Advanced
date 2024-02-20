a20570e42afa69cac5dc6f4d533deb4aebea4747
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-02-20 07:35:34 +0200

    Basic Exam Problem 01 done

---
+++ 00 - Basic Exam/01.Problem/01.Problem/01.Problem/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _01.Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double fat = int.Parse(Console.ReadLine());
+            double prot = int.Parse(Console.ReadLine());
+            double carbon = int.Parse(Console.ReadLine());
+            double totalCal = int.Parse(Console.ReadLine());
+            double water = int.Parse(Console.ReadLine());
+
+            
+            double totalFat = ((fat / 100) * totalCal) / 9;
+            double totalProt = ((prot / 100) * totalCal) / 4;
+            double totalarbon = ((carbon / 100) * totalCal) / 4;
+
+            double foodTotal  = totalFat + totalProt + totalarbon;
+
+            double calPerGram = totalCal / foodTotal;
+            double calRusult =  calPerGram * ((100 - water)/100 );
+
+            Console.WriteLine($"{calRusult:f4}");
+        }
+    }
+}
